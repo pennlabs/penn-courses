4344ae94e86e11ca9b2df5dc2560bf50799ee95d
Davis Haupt <davis.haupt@gmail.com>
2019-05-07 22:20:08 -0400

    remove alert code for now, since it'll still be changing in its own repo

--- alert/urls.py
+++ alert/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('courses', views.get_sections, name='courses'),
-    path('submitted', views.register, name='register'),
-    path('resubscribe/<int:id_>', views.resubscribe, name='resubscribe'),
-    path('webhook', views.accept_webhook, name='webhook'),
+
 ]
