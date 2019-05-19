0f1d2dc2d513ca24e23897f0a53c7ead50e23720
Davis Haupt <davis.haupt@gmail.com>
2019-05-19 18:03:21 -0400

    changed default settings to dev

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PennCourses.settings.base')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PennCourses.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
