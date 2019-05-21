56da67edb62d1a220b2068395a2dc6d4da40af35
Davis Haupt <davis.haupt@gmail.com>
2019-05-21 12:14:57 -0400

    working on review model to import from api

--- review/models.py
+++ review/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ReviewAverage(models.Model):
+    section_id = models.CharField(max_length=16)
+    instructor = models.ForeignKey('courses.Instructor', on_delete=models.CASCADE)
+
+    # review fields
+    course_quality = models.DecimalField(max_digits=3, decimal_places=2)
+    difficulty = models.DecimalField(max_digits=3, decimal_places=2)
+    instructor_quality = models.DecimalField(max_digits=3, decimal_places=2)
+    work_required = models.DecimalField(max_digits=3, decimal_places=2)
+
+    class Meta:
+        unique_together = (('section_id', 'instructor'), )
