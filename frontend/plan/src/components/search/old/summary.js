14421216b5ae0ea42e6f3d8237ed0f69323e87ae
Ying Xuan <engyingxuan@gmail.com>
2019-05-27 10:10:22 -0400

    Shifted old code to different folder

--- frontend/plan/src/components/search/old/summary.js
+++ frontend/plan/src/components/search/old/summary.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import connect from "react-redux/es/connect/connect";
 import PropTypes from "prop-types";
-import { Dropdown } from "../dropdown";
+import { Dropdown } from "../../old/dropdown";
 
 class SummaryDropdown extends Component {
     computeStats = () => {
