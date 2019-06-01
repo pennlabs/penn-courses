b0d71c7411c4337c8461379bfa35081ae6638aca
Davis Haupt <davis.haupt@gmail.com>
2019-06-01 15:39:12 -0400

    code for loading in all review data

--- review/import_reviews.py
+++ review/import_reviews.py
@@ -14,16 +14,6 @@ token = settings.PCR_TOKEN
 base_url = 'https://api.penncoursereview.com/v1'
 
 
-def get_depts():
-    r = requests.get(base_url + '/depts/', {'token': token})
-    return [d['id'] for d in r.json()['result']['values']]
-
-
-def load_data(fname='review/cis-reviews-test.json'):
-    with open(fname) as f:
-        return json.loads(f.read())
-
-
 def extract_reviews(data, department):
     bit_re = re.compile(r'(^r)?([A-Z])')
 
@@ -41,7 +31,9 @@ def extract_reviews(data, department):
     return result
 
 
-def save_reviews(revs):
+def save_reviews(revs, print_every=20):
+    total = len(revs)
+    i = 0
     for rev in revs:
         instr, _ = Instructor.objects.get_or_create(name__contains=rev['instructor'],
                                                     defaults={'name': rev['instructor'].title()})
@@ -50,16 +42,37 @@ def save_reviews(revs):
         review, _ = Review.objects.get_or_create(instructor=instr,
                                                  section=sec)
         review.set_scores(rev['ratings'])
+        i += 1
+        if print_every > 0 and i % print_every == 0:
+            print(f'{i} / {total} reviews processed.')
+
+
+def get_depts():
+    r = requests.get(base_url + '/depts/', {'token': token})
+    if r.status_code == 200:
+        return [d['id'] for d in r.json()['result']['values']]
+
+    return []
 
 
 def get_reviews_for_department(dept):
-    r = requests.get(base_url + f'/depts/{dept}/reviews/')
-    return extract_reviews(r.json(), dept)
+    r = requests.get(base_url + f'/depts/{dept}/reviews/', {'token': token})
+    if r.status_code == 200:
+        return extract_reviews(r.json()['result'], dept)
+    else:
+        print(r.text)
 
 
-def load_reviews():
+def load_reviews_for_department(dept):
+    save_reviews(get_reviews_for_department(dept))
+
+
+def load_all_reviews():
     reviews = []
     depts = get_depts()
+    i = 0
     for dept in depts:
+        i += 1
+        print(f'loading {dept} reviews... ({i} / {len(depts)})')
         reviews.extend(get_reviews_for_department(dept))
     save_reviews(reviews)
