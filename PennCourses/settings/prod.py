690e4b315cd1a76cb00bc2282761d43cc5367859
Davis Haupt <davis.haupt@gmail.com>
2019-09-21 12:23:34 -0400

    added sentry

--- PennCourses/settings/prod.py
+++ PennCourses/settings/prod.py
@@ -1,4 +1,7 @@
 from .base import *
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.celery import CeleryIntegration
 
 DEBUG = False
 
@@ -14,3 +17,9 @@ HOST_TO_APP = {
 
 if len(ALLOWED_HOSTS) == 0:
     ALLOWED_HOSTS = HOST_TO_APP.keys()
+
+sentry_sdk.init(
+    dsn='https://a8da22bb171b439b9a8a8d7fb974d840:a9d22ba68b2644c6ac1a3b325f5cd13a@sentry.pennlabs.org/14',
+    integrations=[DjangoIntegration(), CeleryIntegration()]
+)
+
