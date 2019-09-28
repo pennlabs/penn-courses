9ae56ef68b224727701ebceaee58c9fb75e175c4
Davis Haupt <davis.haupt@gmail.com>
2019-09-28 16:58:43 -0400

    fixed new old typo

--- alert/migrations/0005_delete_courseupdate.py
+++ alert/migrations/0005_delete_courseupdate.py
@@ -8,11 +8,11 @@ def forwards(apps, schema_editor):
     StatusUpdate = apps.get_model('courses', 'StatusUpdate')
     for old_up in CourseUpdate.objects.all():
         new_up = StatusUpdate.objects.create(section=old_up.section,
-                                             old_status = old_up.old_status,
-                                             new_status = old_up.old_status,
-                                             created_at = old_up.created_at,
-                                             alert_sent = old_up.alert_sent,
-                                             request_body = old_up.request_body)
+                                             old_status=old_up.old_status,
+                                             new_status=old_up.new_status,
+                                             created_at=old_up.created_at,
+                                             alert_sent=old_up.alert_sent,
+                                             request_body=old_up.request_body)
         new_up.save()
 
 def backwards(apps, schema_editor):
