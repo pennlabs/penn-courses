7222bcdbca9ff81496e801712271e285247e12c2
dlike230 <dlike@seas.upenn.edu>
2019-10-12 00:10:10 -0400

    Refactored CartSection code and split into several components

--- frontend/plan/src/components/CartSection.js
+++ frontend/plan/src/components/CartSection.js
@@ -2,82 +2,90 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "../styles/course-cart.css";
 
-class CartSection extends Component {
+const CourseDetails = ({ name, code }) => {
+    return <div style={{
+        flexGrow: "2",
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "70%",
+        textAlign: "left",
+        alignItems: "left",
+    }}
+    >
+        <h4>{code}</h4>
+        <div style={{ fontSize: "0.6rem" }}>{name}</div>
+    </div>;
+};
 
-    toggleCheck = () => {
-        this.props.toggleCheck();
+CourseDetails.propTypes = {
+    name: PropTypes.string.isRequired,
+    code: PropTypes.string.isRequired
+};
+
+
+const CourseCheckbox = ({ checked }) => {
+    const checkStyle = {
+        width: "1rem",
+        height: "1rem",
+        borderRadius: "1rem",
+        backgroundColor: "white",
+        border: "1px solid grey",
     };
+    return <div style={{
+        flexGrow: "0",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+    }}
+    >
+        {checked
+            ? (
+                <i
+                    className="fas fa-check-circle"
+                    style={
+                        {
+                            ...checkStyle,
+                            border: "none",
+                            color: "#878ED8",
+                        }
+                    }
+                />
+            )
+            : <div style={checkStyle}/>}
+    </div>;
+};
 
-    render() {
-        const checkStyle = {
-            width: "1rem",
-            height: "1rem",
-            borderRadius: "1rem",
-            backgroundColor: "white",
-            border: "1px solid grey",
-        };
+CourseCheckbox.propTypes = {
+    checked: PropTypes.bool
+};
 
-        const {checked, code, name} = this.props;
-
-        return (
-            <div
-                className="course-cart-item"
-                style={
-                    {
-                        display: "flex",
-                        flexDirection: "row",
-                        justifyContent: "space-around",
-                        padding: "0.8rem",
-                        borderBottom: "1px solid rgb(200, 200, 200)",
-                    }}
-                onClick={this.toggleCheck}
-                role={"checkbox"}
-            >
-                <div style={{
-                    flexGrow: "2",
+const CartSection = ({ toggleCheck, checked, code, name }) => {
+    return (
+        <div
+            className="course-cart-item"
+            style={
+                {
                     display: "flex",
-                    flexDirection: "column",
-                    maxWidth: "70%",
-                    textAlign: "left",
-                    alignItems: "left",
-                }}
-                >
-                    <h4>{code}</h4>
-                    <div style={{ fontSize: "0.6rem" }}>{name}</div>
-                </div>
-                <div style={{
-                    flexGrow: "0",
                     flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    display: "flex",
+                    justifyContent: "space-around",
+                    padding: "0.8rem",
+                    borderBottom: "1px solid rgb(200, 200, 200)",
                 }}
-                >
-                    {checked
-                        ? (
-                            <i
-                                className="fas fa-check-circle"
-                                style={
-                                    {
-                                        ...checkStyle,
-                                        border: "none",
-                                        color: "#878ED8",
-                                    }
-                                }
-                            />
-                        )
-                        : <div style={checkStyle} />}
-                </div>
-            </div>
-        );
-    }
-}
+            onClick={toggleCheck}
+            role="checkbox"
+        >
+            <CourseDetails name={name} code={code}/>
+            <CourseCheckbox checked={checked}/>
+        </div>
+    );
+};
 
 CartSection.propTypes = {
     name: PropTypes.string.isRequired,
     code: PropTypes.string.isRequired,
     checked: PropTypes.bool,
-    toggleCheck: PropTypes.func.isRequired
+    toggleCheck: PropTypes.func.isRequired,
 };
 
 export default CartSection;
