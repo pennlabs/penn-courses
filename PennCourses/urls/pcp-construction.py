9c576fb0f6c6df8acd91f70c4e954b7240aa4a91
Davis Haupt <davis.haupt@gmail.com>
2019-10-12 10:27:29 -0400

    removed ambiguity on template routes

--- PennCourses/urls/pcp-construction.py
+++ PennCourses/urls/pcp-construction.py
@@ -5,5 +5,5 @@ from PennCourses.urls.base import urlpatterns
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='plan/index.html')),
+    path('', TemplateView.as_view(template_name='plan_construction/index.html')),
 ] + urlpatterns
