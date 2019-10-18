4e27be9472ebf3a39d5dc8f66e4ea348411133c8
Daniel Tao <danieltaox@gmail.com>
2019-10-17 22:51:24 -0400

    edit CourseInfo styles

--- frontend/plan/src/components/selector/CourseInfo.js
+++ frontend/plan/src/components/selector/CourseInfo.js
@@ -6,15 +6,15 @@ import SectionList from "./SectionList";
 export default function CourseInfo({ course, back }) {
     return (
         <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
-            <div className="segment" style={{ maxHeight: "10%" }}>
-                <button type="button" className="button is-white" onClick={back} style={{ fontSize: "1em" }}>
+            <div style={{ maxHeight: "10%" }}>
+                <button type="button" className="button back-button grey-text" onClick={back} style={{ fontSize: "1em" }}>
                     <span className="icon">
                         <i className="fas fa-arrow-left" />
                     </span>
                     &nbsp; Back
                 </button>
             </div>
-            <div style={{ margin: ".5em", maxHeight: "40%" }}>
+            <div style={{ margin: ".5em .5em .5em 2em", maxHeight: "40%" }}>
                 <h3 className="title is-4">{course.id}</h3>
                 <h5 className="subtitle is-6">{course.title}</h5>
                 <CourseDetails course={course} />
