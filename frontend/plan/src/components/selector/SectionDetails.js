776c5516f1a73b068b056c56d058b55e37e93527
Daniel Tao <danieltaox@gmail.com>
2019-10-18 18:18:16 -0400

    change section dropdown details section format

--- frontend/plan/src/components/selector/SectionDetails.js
+++ frontend/plan/src/components/selector/SectionDetails.js
@@ -3,19 +3,25 @@ import PropTypes from "prop-types";
 
 import { getTimeString } from "../../meetUtil";
 
+const getSectionId = id => (tokens => tokens[tokens.length - 1])(id.split("-"));
+
 export default function SectionDetails({ section, isOpen }) {
+    const { id, instructors, meetings } = section;
     return (
         <li style={{ display: isOpen ? "block" : "none" }}>
-            <div style={{ margin: ".5em .5em .5em 3em", maxHeight: "40%" }}>
-                <h3 className="title is-5">{section.id}</h3>
-                <h5 className="subtitle is-6">{section.instructors.join(", ")}</h5>
+            <div style={{ margin: ".5em .5em .5em 2em", maxHeight: "40%" }}>
+                <h3 className="title is-6 section-details-title">
+                    <b>{`${getSectionId(id)}`}</b>
+                    &nbsp;&nbsp;
+                    {`${instructors.join(", ")}`}
+                </h3>
                 <i className="far fa-clock" />
                 &nbsp;
-                {getTimeString(section.meetings)}
+                {getTimeString(meetings)}
                 &nbsp;&nbsp;
                 <i className="fas fa-map-marker-alt" />
                 &nbsp;
-                {section.meetings.map(m => m.room).join(", ")}
+                {meetings.map(m => m.room).join(", ")}
             </div>
         </li>
     );
