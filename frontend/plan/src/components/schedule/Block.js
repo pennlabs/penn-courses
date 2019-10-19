c2ee07acbe41edd587669d7e14c911ac73ccb231
Davis Haupt <davis.haupt@gmail.com>
2019-10-19 19:14:45 -0400

    fixed conflicts on safari (30% of PCS users)

--- frontend/plan/src/components/schedule/Block.js
+++ frontend/plan/src/components/schedule/Block.js
@@ -12,35 +12,37 @@ export default function Block(props) {
         gridRowStart: (start - offsets.time) * 2 + offsets.row + 1,
         gridRowEnd: (end - offsets.time) * 2 + offsets.col + 1,
         gridColumn: days.indexOf(day) + 1 + offsets.col,
-        position: "relative",
     };
     return (
-        <div
-            role="button"
-            className={`block ${color}`}
-            style={{ ...pos, ...style }}
-            onClick={focusSection}
-        >
-            <div className="inner-block">
-                <span
-                    role="button"
-                    className="remove"
-                    onClick={(e) => {
-                        remove();
-                        e.stopPropagation();
-                    }}
-                >
-                    <i className="fas fa-times" />
-                </span>
-                <span
-                    className={coreqFulfilled ? "hide" : ""}
-                    title="Registration is required for an associated section."
-                >
-                    <i className="fas fa-exclamation warning" />
-                </span>
-                <span>{id.replace(/-/g, " ")}</span>
+        <div style={{ ...pos }}>
+            <div
+                role="button"
+                className={`block ${color}`}
+                style={{ ...style, position: "relative", height: "100%" }}
+                onClick={focusSection}
+            >
+                <div className="inner-block">
+                    <span
+                        role="button"
+                        className="remove"
+                        onClick={(e) => {
+                            remove();
+                            e.stopPropagation();
+                        }}
+                    >
+                        <i className="fas fa-times" />
+                    </span>
+                    <span
+                        className={coreqFulfilled ? "hide" : ""}
+                        title="Registration is required for an associated section."
+                    >
+                        <i className="fas fa-exclamation warning" />
+                    </span>
+                    <span>{id.replace(/-/g, " ")}</span>
+                </div>
             </div>
         </div>
+
     );
 }
 
