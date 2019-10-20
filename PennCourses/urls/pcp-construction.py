61300a732a5c33782390dfc854bc8d806f38bd80
Davis Haupt <davis.haupt@gmail.com>
2019-10-19 22:48:00 -0400

    we dont need no kubernetes

--- PennCourses/urls/pcp-construction.py
+++ PennCourses/urls/pcp-construction.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 from PennCourses.urls.base import urlpatterns
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='plan_construction/index.html')),
+    path('', RedirectView.as_view(url='https://penncourseplan.com')),
 ] + urlpatterns
