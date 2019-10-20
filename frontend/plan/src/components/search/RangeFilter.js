787a1b739e0c6dcf22cd9666d0344f974f7b53a4
dlike230 <dlike@seas.upenn.edu>
2019-10-20 00:35:12 -0400

    Sliders

--- frontend/plan/src/components/search/RangeFilter.js
+++ frontend/plan/src/components/search/RangeFilter.js
@@ -8,7 +8,17 @@ export function RangeFilter({
     updateRangeFilter, startSearch, rangeProperty, step, isDisabled,
 }) {
     const [searchTimeout, setSearchTimeout] = useState();
+    const [ref, setRef] = useState(null);
+    const [edgeValues, setEdgeValues] = useState([0, 4]);
+
+    const edges = ref && ref.getElementsByClassName("rc-slider-handle");
+    let edge1 = null;
+    let edge2 = null;
+    if (edges) {
+        [edge1, edge2] = edges;
+    }
     const onSliderChange = (value) => {
+        setEdgeValues(value);
         updateRangeFilter(value);
         if (searchTimeout) {
             clearTimeout(searchTimeout);
@@ -21,22 +31,55 @@ export function RangeFilter({
         }, 200));
     };
 
+    edge1 = edge1 && edge1.getBoundingClientRect().top !== 0 && edge1;
+    edge2 = edge2 && edge2.getBoundingClientRect().top !== 0 && edge2;
+
     return (
-        <div className="columns contained is-multiline is-centered">
-            <div className="column is-half">
-                <p>
-                    {" "}
-                    {filterData[rangeProperty][0]}
-                    {" "}
+        <div
+            className="columns contained is-multiline is-centered"
+            style={{ paddingBottom: "20px" }}
+            ref={refVal => setRef(refVal)}
+        >
+            {edge1 && (
+                <p style={{
+                    position: "fixed",
+                    top: `${edge1.getBoundingClientRect().top + 20}px`,
+                    left: `${edge1.getBoundingClientRect().left}px`,
+                }}
+                >
+                    <b>{edgeValues[0]}</b>
                 </p>
-            </div>
-            <div className="column is-half">
-                <p>
-                    {" "}
-                    {filterData[rangeProperty][1]}
-                    {" "}
+            )}
+            {!edge1 && (
+                <p style={{
+                    position: "absolute",
+                    bottom: "10px",
+                    left: "9px",
+                }}
+                >
+                    <b>{edgeValues[0]}</b>
                 </p>
-            </div>
+            )}
+            {edge2 && (
+                <p style={{
+                    position: "fixed",
+                    top: `${edge2.getBoundingClientRect().top + 20}px`,
+                    left: `${edge2.getBoundingClientRect().left}px`,
+                }}
+                >
+                    <b>{edgeValues[1]}</b>
+                </p>
+            )}
+            {!edge2 && (
+                <p style={{
+                    position: "absolute",
+                    bottom: "10px",
+                    right: "9px",
+                }}
+                >
+                    <b>{edgeValues[1]}</b>
+                </p>
+            )}
             <div className="column is-full">
                 <Range
                     min={minRange}
