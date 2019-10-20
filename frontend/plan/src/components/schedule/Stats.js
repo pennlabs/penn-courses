60d1dc5ae205167571946467892a27236ec29ca2
Shaya Zarkesh <mathkidsz@gmail.com>
2019-10-20 16:58:08 -0400

    fixed time stats

--- frontend/plan/src/components/schedule/Stats.js
+++ frontend/plan/src/components/schedule/Stats.js
@@ -12,27 +12,21 @@ const purpleTimeStats = {
 };
 
 class Stats extends Component {
-    parseTime = (time) => {
-        if (isNaN(time) || time === "NaN") {
-            return "N/A";
+    parseTime = (t) => {
+        let hour = Math.floor(t % 12);
+        let min = Math.round((t % 1) * 100);
+        if (hour === 0) {
+            hour = 12;
         }
-        if (time >= 12) {
-            if (time - Math.floor(time) === 0) {
-                return `${time - 12}:00 PM`;
-            }
-
-            return `${(time - 12) + Math.round(60 * (time - Math.floor(time)))}PM`;
+        if (min === 0) {
+            min = "00";
         }
-
-        if (time - Math.floor(time) === 0) {
-            return `${time}:00 AM`;
-        }
-
-        return `${(time) + Math.round(60 * (time - Math.floor(time)))}AM`;
+        return `${hour}:${min} ${t >= 12 ? "PM" : "AM"}`;
     }
 
     render() {
         const { meetings } = this.props;
+        console.log(meetings);
         const startTimes = [];
         const endTimes = [];
         const hoursPerDay = [0, 0, 0, 0, 0];
@@ -57,22 +51,22 @@ class Stats extends Component {
             if (str) {
                 const course = str.substring(0, str.indexOf("-", str.indexOf("-") + 1)); // finds course (irrespective of section)
                 if (course in courseDifficulties) {
-                    courseDifficulties[course] += (section.difficulty ? section.difficulty : 2.0);
+                    courseDifficulties[course] += (section.difficulty ? section.difficulty : 2.5);
                     courseWorkloads[course] += (section.work_required
-                        ? section.work_required : 2.0);
+                        ? section.work_required : 2.5);
                     courseInstructorQualities[course] += (section.instructor_quality
-                        ? section.instructor_quality : 2.0);
+                        ? section.instructor_quality : 2.5);
                     courseQualities[course] += (section.course_quality
-                        ? section.course_quality : 2.0);
+                        ? section.course_quality : 2.5);
                     courseCUs[course] += (section.credits ? section.credits : 1);
                     courseRepeats[course] += 1;
                 } else {
-                    courseDifficulties[course] = (section.difficulty ? section.difficulty : 2.0);
-                    courseWorkloads[course] = (section.work_required ? section.work_required : 2.0);
+                    courseDifficulties[course] = (section.difficulty ? section.difficulty : 2.5);
+                    courseWorkloads[course] = (section.work_required ? section.work_required : 2.5);
                     courseInstructorQualities[course] = (section.instructor_quality
-                        ? section.instructor_quality : 2.0);
+                        ? section.instructor_quality : 2.5);
                     courseQualities[course] = (section.course_quality
-                        ? section.course_quality : 2.0);
+                        ? section.course_quality : 2.5);
                     courseCUs[course] = (section.credits ? section.credits : 1);
                     courseRepeats[course] = 1;
                 }
@@ -99,8 +93,8 @@ class Stats extends Component {
 
         // final computation of stats
 
-        const earliestStart = startTimes ? Math.min(...startTimes) : NaN;
-        const latestEnd = endTimes ? Math.max(...endTimes) : NaN;
+        const earliestStart = Math.min(...startTimes);
+        const latestEnd = Math.max(...endTimes);
 
         const minHoursADay = Math.min(...hoursPerDay);
         const maxHoursADay = Math.max(...hoursPerDay);
@@ -143,49 +137,45 @@ class Stats extends Component {
                         {" "}
                     </div>
                 </div>
-                { false
-                    ? [
-                        <div style={{ display: "grid", gridTemplateRows: "25% 25% 25% 25%" }}>
-                            <div style={{ display: "flex", alignItems: "center" }}>
-                                <div style={purpleTimeStats}>
-                                    {parseFloat(minHoursADay.toFixed(2))}
-                                </div>
-                                <div style={{ fontSize: "0.8em" }}>min hours in a day</div>
-                            </div>
-                            <div style={{ display: "flex", alignItems: "center" }}>
-                                <div style={purpleTimeStats}>
-                                    {parseFloat(maxHoursADay.toFixed(2))}
-                                </div>
-                                <div style={{ fontSize: "0.8em" }}>max hours in a day</div>
-                            </div>
-                            <div style={{ display: "flex", alignItems: "center" }}>
-                                <div style={purpleTimeStats}>
-                                    {parseFloat((totalHours / 5).toFixed(2))}
-                                </div>
-                                <div style={{ fontSize: "0.8em" }}>avg. hours a day</div>
-                            </div>
-                            <div style={{ display: "flex", alignItems: "center" }}>
-                                <div style={purpleTimeStats}>
-                                    {parseFloat(totalHours.toFixed(2))}
-                                </div>
-                                <div style={{ fontSize: "0.8em" }}>total hours of class</div>
-                            </div>
-                        </div>,
-                        <div style={{
-                            padding: "10px", display: "flex", flexDirection: "column", justifyContent: "space-evenly", alignItems: "flex-start",
-                        }}
-                        >
-                            <div>
-                                <div style={{ color: "#7874CF", fontSize: "1.3rem", fontWeight: "bold" }}>{this.parseTime(earliestStart)}</div>
-                                <div>earliest start time</div>
-                            </div>
-                            <div>
-                                <div style={{ color: "#7874CF", fontSize: "1.3rem", fontWeight: "bold" }}>{this.parseTime(latestEnd)}</div>
-                                <div>latest end time</div>
-                            </div>
+                <div style={{ display: "grid", gridTemplateRows: "25% 25% 25% 25%" }}>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                        <div style={purpleTimeStats}>
+                            {parseFloat(minHoursADay.toFixed(1))}
                         </div>
-                    ]
-                    : <span />}
+                        <div style={{ fontSize: "0.8em" }}>min hours in a day</div>
+                    </div>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                        <div style={purpleTimeStats}>
+                            {parseFloat(maxHoursADay.toFixed(1))}
+                        </div>
+                        <div style={{ fontSize: "0.8em" }}>max hours in a day</div>
+                    </div>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                        <div style={purpleTimeStats}>
+                            {parseFloat((totalHours / 5).toFixed(1))}
+                        </div>
+                        <div style={{ fontSize: "0.8em" }}>avg. hours a day</div>
+                    </div>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                        <div style={purpleTimeStats}>
+                            {parseFloat(totalHours.toFixed(1))}
+                        </div>
+                        <div style={{ fontSize: "0.8em" }}>total hours of class</div>
+                    </div>
+                </div>
+                <div style={{
+                    padding: "10px", display: "flex", flexDirection: "column", justifyContent: "space-evenly", alignItems: "flex-start",
+                }}
+                >
+                    <div>
+                        <div style={{ color: "#7874CF", fontSize: "1.3rem", fontWeight: "bold" }}>{this.parseTime(earliestStart)}</div>
+                        <div>earliest start time</div>
+                    </div>
+                    <div>
+                        <div style={{ color: "#7874CF", fontSize: "1.3rem", fontWeight: "bold" }}>{this.parseTime(latestEnd)}</div>
+                        <div>latest end time</div>
+                    </div>
+                </div>
             </div>
         );
     }
