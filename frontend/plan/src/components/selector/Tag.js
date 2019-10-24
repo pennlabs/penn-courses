2b95b92dc60ac01171ddd1562675ddbb5ac14ab5
dlike230 <dlike@seas.upenn.edu>
2019-10-24 17:45:22 -0400

    collapsible requirements

--- frontend/plan/src/components/selector/Tag.js
+++ frontend/plan/src/components/selector/Tag.js
@@ -1,13 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default function Tag({ children, onClick = null }) {
+export default function Tag({ children, onClick = null, isAdder = false }) {
     return (
         <span
             role="button"
             onClick={onClick}
-            className="tag is-rounded is-light detail-tag"
-            style={{ cursor: onClick ? "pointer" : "default" }}
+            className={"tag is-rounded is-light detail-tag" + (isAdder ? " is-adder" : "")}
         >
             {children}
         </span>
@@ -18,4 +17,5 @@ Tag.propTypes = {
     // eslint-disable-next-line
     children: PropTypes.any,
     onClick: PropTypes.func,
+    isAdder: PropTypes.bool,
 };
--- frontend/plan/src/components/selector/TagList.js
+++ frontend/plan/src/components/selector/TagList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Tag from "./Tag";
 
@@ -7,24 +7,28 @@ import "bulma-popover/css/bulma-popver.min.css";
 export default function TagList({
     elements, onClick = null, limit = 1, select = null,
 }) {
+    const [expanded, setExpanded] = useState(false);
     const visibleTags = elements.slice(0, limit);
     const hiddenTags = elements.slice(limit);
     let tagPopover = null;
+    const sign = expanded ? "-" : "+";
+    const adder = <Tag isAdder
+                       onClick={() => setExpanded(!expanded)}>{`${sign}${hiddenTags.length}`}</Tag>;
     if (hiddenTags.length > 0) {
         tagPopover = (
-            <span className="popover is-popover-left">
-                <span className="popover-trigger">
-                    <Tag>{`+${hiddenTags.length}`}</Tag>
-                </span>
-                <span className="popover-content">
+            <span>
+                {!expanded && adder}
+                <span className="taglist" style={{ height: expanded ? "auto" : 0 }}>
                     {hiddenTags.map(elt => <Tag>{elt}</Tag>)}
                 </span>
+                {expanded && adder}
             </span>
         );
     }
     return (
         <span>
-            {visibleTags.map(elt => <Tag onClick={onClick ? () => onClick(elt.replace(/ /g, "-")) : null}>{elt}</Tag>)}
+            {visibleTags.map(elt => <Tag
+                onClick={onClick ? () => onClick(elt.replace(/ /g, "-")) : null}>{elt}</Tag>)}
             {tagPopover}
         </span>
     );
