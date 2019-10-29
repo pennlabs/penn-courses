40706c72090f0f2e097c0689474734231ec91e4f
Davis Haupt <davis.haupt@gmail.com>
2019-10-29 15:54:31 -0400

    remove dead code

--- alert/urls.py
+++ alert/urls.py
@@ -6,7 +6,6 @@ from alert import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('courses', views.get_sections, name='courses'),
     path('courses/', courses.views.SectionList.as_view()),
     path('submitted', views.register, name='register'),
     path('resubscribe/<int:id_>', views.resubscribe, name='resubscribe'),
