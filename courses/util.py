eae8396c9e868dd7d4734124c0af136f788d3e85
Charles Cunningham <mureytasroc@gmail.com>
2019-11-03 14:20:28 -0500

    Online req-res fix

--- courses/util.py
+++ courses/util.py
@@ -112,15 +112,16 @@ def add_college_requirements(course, college_reqs):
         'MDS': 'Society Sector',
         'MDH': 'History & Tradition Sector',
         'MDA': 'Arts & Letters Sector',
-        'MDO,MDB': 'Humanities & Social Science Sector',
+        'MDO': 'Humanities & Social Science Sector',
         'MDL': 'Living World Sector',
         'MDP': 'Physical World Sector',
-        'MDN,MDB': 'Natural Science & Math Sector',
+        'MDN': 'Natural Science & Math Sector',
         'MWC': 'Writing Requirement',
         'MQS': 'College Quantitative Data Analysis Req.',
         'MFR': 'Formal Reasoning Course',
         'MC1': 'Cross Cultural Analysis',
-        'MC2': 'Cultural Diversity in the US'
+        'MC2': 'Cultural Diversity in the US',
+        'MGH': 'Benjamin Franklin Seminars'
     }
     name_to_code = dict([(v, k) for k, v in code_to_name.items()])
     for req_name in college_reqs:
@@ -131,6 +132,15 @@ def add_college_requirements(course, college_reqs):
                                                   'name': req_name
                                                 })[0]
         req.courses.add(course)
+
+
+def relocate_reqs_from_rests(rests, reqs):
+    if any(r.requirement_description == 'Humanities & Social Science Sector' for r in rests):
+        reqs.append('Humanities & Social Science Sector')
+    if any(r.requirement_description == 'Natural Science & Math Sector' for r in rests):
+        reqs.append('Natural Science & Math Sector')
+    if any(r.requirement_description == 'Benjamin Franklin Seminars' for r in rests):
+        reqs.append('Benjamin Franklin Seminars')
 
 
 def upsert_course_from_opendata(info, semester):
@@ -166,6 +176,8 @@ def upsert_course_from_opendata(info, semester):
     set_meetings(section, info['meetings'])
     add_associated_sections(section, info)
     add_restrictions(section, info['requirements'])
+    relocate_reqs_from_rests(info['requirements'],
+                             info['fulfills_college_requirements'].append('Humanities & Social Science Sector'))
     add_college_requirements(section.course, info['fulfills_college_requirements'])
 
     section.save()
