0cf1f1d80b7789039c46a23fe3c68b0965019e17
Davis Haupt <davis.haupt@gmail.com>
2019-12-28 09:34:22 -0500

    add command to load course status

---
+++ courses/management/commands/loadstatus.py
@@ -0,0 +1,17 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from courses.tasks import load_courses, load_requirements, set_all_status
+
+
+class Command(BaseCommand):
+    help = 'Load course status for courses in the DB'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--semester', nargs='?', type=str)
+
+    def handle(self, *args, **kwargs):
+        root_logger = logging.getLogger('')
+        root_logger.setLevel(logging.DEBUG)
+        set_all_status(semester=kwargs['semester'])
