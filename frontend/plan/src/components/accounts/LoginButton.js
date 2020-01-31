210f3028fc712f7dc8a388f5269455f6b55bb8b9
Daniel Tao <dtao@seas.upenn.edu>
2020-01-31 02:10:21 -0500

    convert LoginButton

--- frontend/plan/src/components/accounts/LoginButton.js
+++ frontend/plan/src/components/accounts/LoginButton.js
@@ -1,15 +1,10 @@
 import React from "react";
+import s from "./LoginButton.module.css";
 
 const LoginButton = () => (
     <a
-        className="button is-link login"
+        className={`button is-link ${s.login}`}
         href={`/accounts/login/?next=${window.location}`}
-        style={{
-            padding: "0.5rem",
-            fontSize: "1rem!important",
-            paddingRight: "1rem",
-            paddingLeft: "1rem",
-        }}
     >
         Login
     </a>
