ac58fbee153f6fef42035b7b5f1b096fbe39f590
dlike230 <dlike@seas.upenn.edu>
2020-01-31 17:56:51 -0500

    Basic course clustering

--- courses/management/commands/recommendcourses.py
+++ courses/management/commands/recommendcourses.py
@@ -2,13 +2,14 @@ from typing import Set
 
 from accounts.middleware import User
 from django.core.management.base import BaseCommand
+from sklearn.cluster import KMeans, AgglomerativeClustering
 
 from plan.models import Schedule
 import numpy as np
 import sklearn
 
 
-def encode_sections(sections) -> Set[str]:
+def sections_to_courses(sections) -> Set[str]:
     """
     Encodes a list section objects as a set of the base course code (e.g., CIS-140)
     :param sections: A section object list
@@ -29,16 +30,24 @@ class Command(BaseCommand):
         users = User.objects.all()
         num_users = len(users)
         id_transformer = {user.pk: i for i, user in enumerate(users)}
-        section_vectors = {}
+        course_vectors_dict = {}
         for schedule in Schedule.objects.all():
             vector_component = id_transformer[schedule.person.pk]
-            for section in encode_sections(schedule.sections.all()):
+            for course in sections_to_courses(schedule.sections.all()):
                 relevant_vector: np.ndarray
-                if section not in section_vectors:
+                if course not in course_vectors_dict:
                     relevant_vector = np.zeros(num_users)
-                    section_vectors[section] = relevant_vector
+                    course_vectors_dict[course] = relevant_vector
                 else:
-                    relevant_vector = section_vectors[section]
+                    relevant_vector = course_vectors_dict[course]
                 relevant_vector[vector_component] += 1
 
-        print(section_vectors)
+        courses, course_vectors = zip(*course_vectors_dict.items())
+        courses, course_vectors = list(courses), np.array(list(course_vectors))
+        num_clusters = 3
+        model = AgglomerativeClustering(n_clusters=num_clusters, linkage="average", affinity="cosine")
+        raw_cluster_result = model.fit_predict(course_vectors)
+        clusters = [[] for _ in range(num_clusters)]
+        for course_index, cluster_index in enumerate(raw_cluster_result):
+            clusters[cluster_index].append(courses[course_index])
+        print(clusters)
