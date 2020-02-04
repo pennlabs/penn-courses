1b436499f9fa8a85a90cb03bd958387ba752750e
Davis Haupt <davis.haupt@gmail.com>
2020-02-04 13:59:38 -0500

    fix static files

--- PennCourses/settings/base.py
+++ PennCourses/settings/base.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
