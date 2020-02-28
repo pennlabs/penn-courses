21037fe9dd1aa04f9fcf7aaa7d3584e908a218f7
Ying Xuan <engyingxuan@gmail.com>
2020-02-28 17:20:37 -0500

    Set up eslint and fixed formatting

--- frontend/alert/src/App.js
+++ frontend/alert/src/App.js
@@ -1,7 +1,7 @@
-import React, {useRef} from 'react';
-import './App.css';
-import styled from 'styled-components';
-import Logo from './assets/PCA_logo.svg'
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import Logo from "./assets/PCA_logo.svg";
 
 
 const Container = styled.div`
@@ -10,7 +10,7 @@ const Container = styled.div`
   align-items: center;
   height:100vh;
   background: rgb(251, 252, 255);
-`
+`;
 
 const Title = styled.div`
   width:100%;
@@ -18,22 +18,22 @@ const Title = styled.div`
   background: #FF3860;
   text-align: center;
   color:white;
-`
+`;
 
 const Grid = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   margin-top: 2rem;
-`
+`;
 
 const Tagline = styled.h3`
     color: #4A4A4A;
     font-weight: normal;
-`
+`;
 const Header = styled.h1`
   color: #4A4A4A
-`
+`;
 
 const Input = styled.input`
   box-shadow: 0 4px 8px 0 rgba(200, 200, 200, 0.2), 0 6px 20px 0 rgba(200, 200, 200, 0.1);
@@ -50,12 +50,13 @@ const Input = styled.input`
   ::placeholder{
     color: #D0D0D0;
   }
-`
+`;
 
+// eslint-disable-next-line
 const MiddleArea = styled.div`
   background: rgb(251, 252, 255);
   width: 100%;
-`
+`;
 
 const Footer = styled.div`
   color: #999999;
@@ -68,7 +69,7 @@ const Footer = styled.div`
   padding-top: 3em;
   padding-bottom:3em;
   line-height:1.5;
-`
+`;
 
 const SubmitButton = styled.button`
   border-radius: 5px;
@@ -83,49 +84,49 @@ const SubmitButton = styled.button`
   :hover{
     background-color: #1496ed;
   }
-`
+`;
 const TopBanner = (
     <Title>
-      We're currently closed for signups. Come back after schedules have been posted!
+      We&#39re currently closed for signups. Come back after schedules have been posted!
     </Title>
-  );
+);
 
 const LogoArea = (
-  <Grid>
-    <img width="100px" height="100px" src={require("./assets/PCA_logo.svg")}/>
-    <Header>Penn Course Alert</Header>
-  </Grid>
-)
+    <Grid>
+        <img width="100px" height="100px" src={Logo} />
+        <Header>Penn Course Alert</Header>
+    </Grid>
+);
 
 
 function App() {
-  const onSubmit = () => {console.log(this.CourseInput.value)};
-  return (
-    <Container>
-        {TopBanner}
-        {LogoArea}
-        <Tagline>Get alerted when a course opens up, by text and email.</Tagline>
-        <Input autocomplete="off" placeholder="Course"></Input>
-        <Input placeholder="Email"></Input>
-        <Input placeholder="Phone"></Input>
-        <SubmitButton onClick={onSubmit}>Submit</SubmitButton>
-        <Footer>
+    const onSubmit = () => { };
+    return (
+        <Container>
+            {TopBanner}
+            {LogoArea}
+            <Tagline>Get alerted when a course opens up, by text and email.</Tagline>
+            <Input autocomplete="off" placeholder="Course" />
+            <Input placeholder="Email" />
+            <Input placeholder="Phone" />
+            <SubmitButton onClick={onSubmit}>Submit</SubmitButton>
+            <Footer>
           Made with
                 {" "}
                 <span className="icon is-small"><i className="fa fa-heart" style={{ color: "red" }} /></span>
                 {" "}
                 by
                 {" "}
-                <a href="http://pennlabs.org" target="_blank">Penn Labs</a>
+                <a href="http://pennlabs.org" rel="noopener noreferrer" target="_blank">Penn Labs</a>
                 {" "}
                 .
                 <br />
                 Have feedback about Penn Course Alert? Let us know
                 {" "}
                 <a href="https://airtable.com/shra6mktROZJzcDIS">here!</a>
-        </Footer>
-    </Container>
-  );
+            </Footer>
+        </Container>
+    );
 }
 
 export default App;
