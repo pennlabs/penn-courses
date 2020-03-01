f16d9a2857a6b0fc4fc077bb42c92ddbad026e2d
szarkesh <44183438+szarkesh@users.noreply.github.com>
2020-03-01 12:37:50 -0500

    updated to jess's designs (#180)
    * updated to jess's designs
    
    * refactor into more components
    
    Co-authored-by: mathkidsz <shaya@polyup.com>
    Co-authored-by: Davis Haupt <davis.haupt@gmail.com>

--- frontend/alert/src/App.js
+++ frontend/alert/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import styled from "styled-components";
 import Logo from "./assets/PCA_logo.svg";
 
+import {maxWidth, minWidth, PHONE} from "./constants"
 
 const Container = styled.div`
   display: flex;
@@ -20,11 +21,10 @@ const Title = styled.div`
   color:white;
 `;
 
-const Grid = styled.div`
+const Flex = styled.div`
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-top: 2rem;
+  flex-direction: ${props => props.col ? 'column' : 'row'};
+  align-items: ${props => props.align || 'center'};
 `;
 
 const Tagline = styled.h3`
@@ -32,17 +32,20 @@ const Tagline = styled.h3`
     font-weight: normal;
 `;
 const Header = styled.h1`
-  color: #4A4A4A
+  color: #4A4A4A;
+
+  ${maxWidth(PHONE)} {
+    font-size: 1.5rem;
+  }
 `;
 
 const Input = styled.input`
-  box-shadow: 0 4px 8px 0 rgba(200, 200, 200, 0.2), 0 6px 20px 0 rgba(200, 200, 200, 0.1);
   outline: none;
-  border: none;
+  border: 1px solid #d6d6d6;
   color: #4A4A4A;
   font-size: 1.4rem;
   padding: 0.5rem 1rem;
-  width: 320px;
+  border-radius: 5px;
   margin: 0.6rem;
   :focus {
     box-shadow: 0 0 0 0.125em rgba(50,115,220,.25)
@@ -50,13 +53,19 @@ const Input = styled.input`
   ::placeholder{
     color: #D0D0D0;
   }
+
+  ${maxWidth(PHONE)} {
+    max-width: 320px;
+  }
+
+  ${minWidth(PHONE)} {
+    width: 320px;
+  }
 `;
 
-// eslint-disable-next-line
-const MiddleArea = styled.div`
-  background: rgb(251, 252, 255);
-  width: 100%;
-`;
+const Center = styled.div`
+  text-align: center;
+`
 
 const Footer = styled.div`
   color: #999999;
@@ -65,7 +74,6 @@ const Footer = styled.div`
   position: absolute;
   bottom: 15px;
   width: 100%;
-  background: white;
   padding-top: 3em;
   padding-bottom:3em;
   line-height:1.5;
@@ -77,7 +85,8 @@ const SubmitButton = styled.button`
   color: white;
   font-size:1em;
   margin:1em;
-  padding: 0.5em 1em;
+  width: 5em;
+  padding: 0.7em 1em;
   transition: 0.2s all;
   border: none;
   cursor: pointer;
@@ -85,32 +94,89 @@ const SubmitButton = styled.button`
     background-color: #1496ed;
   }
 `;
+
+const Toast = styled.div`
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  max-width: 300px;
+  /* background-color: */
+`;
+
 const TopBanner = (
     <Title>
       We&#39;re currently closed for signups. Come back after schedules have been posted!
     </Title>
 );
 
-const LogoArea = (
-    <Grid>
-        <img width="100px" height="100px" src={Logo} />
-        <Header>Penn Course Alert</Header>
-    </Grid>
-);
+const LogoArea = () => (
+  <Flex>
+    <img width="70px" height="70px" src={Logo}/>
+    <Header>Penn Course Alert</Header>
+  </Flex>
+)
+
+
+const NavContainer = styled.nav`
+  margin: 20px;
+  display: flex;
+  flex-align: left;
+  width: 95%;
+`;
+
+const NavElt = styled.a`
+  padding: 20px;
+  color: #4a4a4a;
+  font-weight: ${props => props.href === ("/" + window.location.href.split("/")[window.location.href.split("/").length - 1]) ? "bold" : "normal"}
+  `
+
+const AlertText = styled.div`
+  padding-top: 1rem;
+  color: #555555;
+`
+const Dropdown = styled.span`
+  color: #4a4a4a;
+  cursor: pointer;
+  font-weight: bold;
+`;
+
+const Nav = () => (
+  <NavContainer>
+    <NavElt href="/">Home</NavElt>
+    <NavElt href="/manage">Manage Alerts</NavElt>
+  </NavContainer>
+)
+
+const Heading = () => (
+  <Center>
+    <LogoArea />
+    <Tagline>Get alerted when a course opens up.</Tagline>
+  </Center>
+)
+
+const AlertForm = ({ onSubmit }) => (
+  <>
+    <Input autocomplete="off" placeholder="Course"></Input>
+    <Input placeholder="Email"></Input>
+    <Input placeholder="Phone"></Input>
+    <Center>
+      <AlertText>Alert me <Dropdown>until I cancel</Dropdown></AlertText>
+      <SubmitButton onClick={onSubmit}>Submit</SubmitButton>
+    </Center>
+  </>
+)
 
 
 function App() {
     const onSubmit = () => { };
     return (
-        <Container>
-            {TopBanner}
-            {LogoArea}
-            <Tagline>Get alerted when a course opens up, by text and email.</Tagline>
-            <Input autocomplete="off" placeholder="Course" />
-            <Input placeholder="Email" />
-            <Input placeholder="Phone" />
-            <SubmitButton onClick={onSubmit}>Submit</SubmitButton>
-            <Footer>
+    <Container>
+        <Nav />
+        <Flex col>
+            <Heading />
+            <AlertForm onSubmit={onSubmit}/>
+        </Flex>
+        <Footer>
           Made with
                 {" "}
                 <span className="icon is-small"><i className="fa fa-heart" style={{ color: "red" }} /></span>
---
+++ frontend/alert/src/constants.js
@@ -0,0 +1,8 @@
+export const DESKTOP = '1248px'
+export const TABLETh = '992px'
+export const PHONE = '584px'
+
+export const minWidth = w =>
+  `@media screen and (min-width: ${w})`
+export const maxWidth = w =>
+  `@media screen and (max-width: ${w})`
