6789f8540175d54287f2d6d3222114781b687c75
Davis Haupt <davis.haupt@gmail.com>
2020-04-10 12:11:02 -0400

    dockerfiles should build now

---
+++ frontend/alert/src/components/shared/accounts/LoginButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "bulma/css/bulma.css";
+import styled from "styled-components";
+
+const LoginButtonStyles = styled.a`
+    font-size: 0.85rem !important;
+    padding: 0 1rem 0 1rem;
+    margin-right: 1rem;
+`;
+
+const LoginButton = () => (
+    <LoginButtonStyles
+        className="button is-link login"
+        href={`/accounts/login/?next=${window.location.pathname}`}
+        style={{
+            padding: "0.5rem",
+            fontSize: "1rem!important",
+            paddingRight: "1rem",
+            paddingLeft: "1rem",
+        }}
+    >
+        Login
+    </LoginButtonStyles>
+);
+
+export default LoginButton;
