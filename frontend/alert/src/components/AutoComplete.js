2b63a88f53c5b56c3fcad7b920374e3a595a806b
dlike230 <dlike@seas.upenn.edu>
2020-04-13 13:49:40 -0400

    adjust suggestion ux

--- frontend/alert/src/components/AutoComplete.js
+++ frontend/alert/src/components/AutoComplete.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import AwesomeDebouncePromise from "awesome-debounce-promise";
+import { useOnClickOutside } from "./shared/useOnClickOutside";
 
 const suggestionsFor = (search) => fetch(`/api/alert/courses?search=${search}`)
     .then(res => res.json());
@@ -12,11 +13,15 @@ const suggestionsDebounced = AwesomeDebouncePromise(
 
 const Suggestions = styled.div`
     position: absolute;
-    left: ${({below}) => below.getBoundingClientRect().left}px;
-    top: ${({below}) => below.getBoundingClientRect().bottom}px;
-    width: ${({below}) => below.getBoundingClientRect().width}px;
+    left: ${({ below }) => below && below.getBoundingClientRect().left}px;
+    top: ${({ below }) => below && below.getBoundingClientRect().bottom}px;
+    width: ${({ below }) => below && below.getBoundingClientRect().width}px;
+    visibility: ${({ hidden }) => hidden ? "hidden" : "visible"};
+    max-height: 20rem;
+    overflow-y: scroll;
     background-color: white;
     padding-top: 0.5rem;
+    z-index:5000;
     padding-bottom: 0.5rem;
     border-color: grey;
     border-width: 1px;
@@ -27,22 +32,29 @@ const Suggestions = styled.div`
 const AutoComplete = ({ children }) => {
     const inputRef = useRef();
     const [suggestions, setSuggestions] = useState([]);
-    const childWithRef = React.cloneElement(children, { ref: inputRef });
+    const [active, setActive] = useState(false);
+    const childWithRef = React.cloneElement(children, {
+        ref: inputRef,
+        onClick: () => setActive(true)
+    });
 
     useEffect(() => {
         if (inputRef.current) {
-            inputRef.current.addEventListener("keyup", ({target: {value}}) => {
-                suggestionsDebounced(value).then(setSuggestions);
+            inputRef.current.addEventListener("keyup", ({ target: { value } }) => {
+                suggestionsDebounced(value)
+                    .then(setSuggestions);
             });
         }
     }, [inputRef]);
 
-    return <>
+    const show = active && suggestions.length > 0;
+
+    return <div ref={useOnClickOutside(() => setActive(false), !show)}>
         {childWithRef}
-        <Suggestions below={inputRef.current}>
-            {suggestions.map(suggestion => <p>{suggestion}</p>)}
-        </Suggestions>
-    </>;
+        {<Suggestions below={inputRef.current} hidden={!show}>
+            {suggestions.map(suggestion => <p>{suggestion.section_id}</p>)}
+        </Suggestions>}
+    </div>;
 };
 
 export default AutoComplete;
