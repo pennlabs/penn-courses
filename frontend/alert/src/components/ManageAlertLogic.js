97947f52b594a0a48dd7299eca26823970404dd8
Ying Xuan <engyingxuan@gmail.com>
2020-04-18 17:19:04 -0400

    Changed query to get status directly from api call

--- frontend/alert/src/components/ManageAlertLogic.js
+++ frontend/alert/src/components/ManageAlertLogic.js
@@ -13,77 +13,48 @@ const getStatusPromise = (sectionId) => {
 
 const processAlerts = (setAlerts) => {
     fetchAlerts()
-        .then((res) => {
-            // res is an array of JSON objects, each with information about
-            // an alert
+        .then(res => (
+            res.map((section) => {
+                let datetime = null;
+                if (section.notification_sent) {
+                    const date = Intl.DateTimeFormat("en-US")
+                        .format(new Date(
+                            section.notification_sent_at
+                        ));
+                    const time = Intl.DateTimeFormat("en-US", {
+                        hour: "numeric",
+                        minute: "numeric",
+                        hour12: true,
+                    }).format(new Date(section.notification_sent_at));
+                    datetime = `${date} at ${time}`;
+                }
 
-            // Initialize a Map mapping each section ID to a
-            // boolean, which represents whether a section is open
-            const sectionStatus = new Map();
+                const status = section.section_status
+                    ? AlertStatus.Open : AlertStatus.Closed;
 
-            // We only want to query each section's status once.
-            let sections = new Set();
-            res.forEach((section) => {
-                sections.add(section.section);
-            });
-            sections = Array.from(sections);
+                let repeat;
+                if (section.is_active) {
+                    if (section.auto_resubscribe) {
+                        repeat = AlertRepeat.EOS;
+                    } else {
+                        repeat = AlertRepeat.Once;
+                    }
+                } else {
+                    repeat = AlertRepeat.Inactive;
+                }
 
-            const promises = [];
-            sections.forEach((section) => {
-                promises.push(getStatusPromise(section));
-            });
-
-            Promise.all(promises)
-                .then(vals => Promise.all(vals.map(val => val.json())))
-                .then((vals) => {
-                    vals.forEach((val) => {
-                        const open = val.status === "O";
-                        sectionStatus.set(val.id, open);
-                    });
-                })
-                .then(() => {
-                    return res.map((section) => {
-                        let datetime = null;
-                        if (section.notification_sent) {
-                            const date = Intl.DateTimeFormat("en-US")
-                                .format(new Date(
-                                    section.notification_sent_at
-                                ));
-                            const time = Intl.DateTimeFormat("en-US", {
-                                hour: "numeric",
-                                minute: "numeric",
-                                hour12: true,
-                            }).format(new Date(section.notification_sent_at));
-                            datetime = `${date} at ${time}`;
-                        }
-
-                        const status = sectionStatus.get(section.section)
-                            ? AlertStatus.Open : AlertStatus.Closed;
-
-                        let repeat;
-                        if (section.is_active) {
-                            if (section.auto_resubscribe) {
-                                repeat = AlertRepeat.EOS;
-                            } else {
-                                repeat = AlertRepeat.Once;
-                            }
-                        } else {
-                            repeat = AlertRepeat.Inactive;
-                        }
-
-                        return {
-                            id: section.id,
-                            section: section.section,
-                            date: datetime,
-                            status,
-                            repeat,
-                            actions: repeat === AlertRepeat.Once
-                                ? AlertAction.Cancel : AlertAction.Resubscribe,
-                        };
-                    });
-                })
-                .then(alerts => setAlerts(alerts));
-        });
+                return {
+                    id: section.id,
+                    section: section.section,
+                    date: datetime,
+                    status,
+                    repeat,
+                    actions: repeat === AlertRepeat.Once
+                        ? AlertAction.Cancel : AlertAction.Resubscribe,
+                };
+            })
+        ))
+        .then(alerts => setAlerts(alerts));
 };
 
 const filterAlerts = (alerts, filter) => (
