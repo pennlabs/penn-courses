937696eab99294a95de16d613de77a0e29098a7b
Charles Cunningham <mureytasroc@gmail.com>
2020-04-19 21:14:59 -0400

    lint

--- backend/alert/migrations/0009_auto_20200419_2112.py
+++ backend/alert/migrations/0009_auto_20200419_2112.py
@@ -6,18 +6,16 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('alert', '0008_registration_original_created_at'),
+        ("alert", "0008_registration_original_created_at"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='registration',
-            name='cancelled',
-            field=models.BooleanField(default=False),
+            model_name="registration", name="cancelled", field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='registration',
-            name='cancelled_at',
+            model_name="registration",
+            name="cancelled_at",
             field=models.DateTimeField(blank=True, null=True),
         ),
     ]
