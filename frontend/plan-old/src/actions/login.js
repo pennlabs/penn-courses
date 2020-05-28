af181a3187db59147e39832a8364da6ac1cab0d2
Ying Xuan <engyingxuan@gmail.com>
2020-05-28 18:14:27 -0400

    bye CRA-plan

--- frontend/plan-old/src/actions/login.js
+++
@@ -1,5 +0,0 @@
-export const LOGIN = "LOGIN";
-export const LOGOUT = "LOGOUT";
-
-export const login = (user) => ({ type: LOGIN, user });
-export const logout = () => ({ type: LOGOUT });
--- frontend/plan-old/src/components/schedule/old/Line.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-import React from "react";
-
-const Line = (props) => (
-    <hr width="99.7%" className="schedline" style={{ top: props.y + "%" }} />
-);
-
-export default Line;
