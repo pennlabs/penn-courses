e382ce2758aefa659057f7c8803f73e181895fbe
Davis Haupt <davis.haupt@gmail.com>
2020-07-14 18:47:03 -0400

    increase sync interval to hopefully reduce load

--- frontend/plan/constants/sync_constants.js
+++ frontend/plan/constants/sync_constants.js
@@ -1,10 +1,10 @@
 // The source of all constants related to schedule syncing
 
 // The minimum interval at which calls can be made to the backend
-export const MIN_FETCH_INTERVAL = 250;
+export const MIN_FETCH_INTERVAL = 2000;
 
 // The interval at which to check whether the frontend and backend are up-to-date with one another
-export const SYNC_INTERVAL = 5000;
+export const SYNC_INTERVAL = 15000;
 
 // Whether to notify the user not to have multiple tabs open
 export const DISABLE_MULTIPLE_TABS = false;
