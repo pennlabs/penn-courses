f280a5276d5bdbf133a89098174107f1fcd7adab
Adam Strike <astrike30@users.noreply.github.com>
2020-09-30 23:27:42 -0400

    Change tasks.py to include close alerts

--- backend/alert/tasks.py
+++ backend/alert/tasks.py
@@ -31,15 +31,19 @@ def send_alert(reg_id, sent_by=""):
     return {"result": result, "task": "pca.tasks.send_alert"}
 
 
-def get_active_registrations(course_code, semester):
+def get_active_registrations(course_code, semester, course_status="O"):
     _, section = get_course_and_section(course_code, semester)
-    return list(section.registration_set.filter(notification_sent=False, deleted=False))
+    # if open then want registrations where the notification has not yet been sent else closed and then want where notification has been sent and where user has requested a close notification
+    if course_status == "O": 
+        return list(section.registration_set.filter(notification_sent=False, deleted=False))
+    else:
+        return list(section.registration_set.filter(close_notification=True, notification_sent=True, deleted=False))
 
 
 @shared_task(name="pca.tasks.send_course_alerts")
-def send_course_alerts(course_code, semester=None, sent_by=""):
+def send_course_alerts(course_code, semester=None, sent_by="", course_status="O"):
     if semester is None:
         semester = get_value("SEMESTER")
 
-    for reg in get_active_registrations(course_code, semester):
+    for reg in get_active_registrations(course_code, semester, course_status=course_status):
         send_alert.delay(reg.id, sent_by)
