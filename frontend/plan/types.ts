c29a453f8ca304f426f43508a03760f2d64d8fed
haoyxc <48307028+haoyxc@users.noreply.github.com>
2020-10-04 22:19:19 -0400

    add types from api

--- frontend/plan/types.ts
+++ frontend/plan/types.ts
@@ -1,11 +1,45 @@
 export interface Section {
     id: string;
+    status: Status;
+    activity: Activity;
+}
+
+export interface Meeting {
+    day: string;
+    start: number;
+    end: number;
+    room: string;
+}
+
+export interface Profile {
+    email: string | null;
+    phone: string | null;
 }
 export enum School {
     SEAS = "SEAS",
     WHARTON = "WH",
     COLLEGE = "SAS",
     NURSING = "NUR",
+}
+
+export enum Status {
+    OPEN = "O",
+    CLOSED = "C",
+    CANCELLED = "X",
+    UNLISTED = "",
+}
+
+export enum Activity {
+    CLINIC = "CLN",
+    DISSERTATION = "DIS",
+    INDEPENDENT_STUDY = "IND",
+    LAB = "LAB",
+    MASTERS_THESIS = "MST",
+    RECITATION = "REC",
+    SEMINAR = "SEM",
+    SENIOR_THESIS = "SRT",
+    STUDIO = "STU",
+    UNDEFINED = "***",
 }
 export interface Requirement {
     id: string;
@@ -28,4 +62,28 @@ export interface Course {
     work_required: number;
     crosslistings: string[];
     requirements: Requirement[];
+}
+
+export interface Schedule {
+    id: string;
+    sections: Section[];
+    credits: number;
+    semester: string;
+    meetings: Meeting[];
+    instructors: string[];
+    course_quality: number;
+    instructor_quality: number;
+    difficulty: number;
+    work_required: number;
+    associated_sections: Section[];
+    name: string;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface User {
+    username: string;
+    first_name: string;
+    last_name: string;
+    profile: Profile;
 }
