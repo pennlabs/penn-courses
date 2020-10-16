70fa7c433c42ec92a306897b322e71212e6a65c0
Cindy <48307028+haoyxc@users.noreply.github.com>
2020-10-16 13:18:53 -0500

    Ts types (#234)
    * partial CourseInfo transition
    
    * add types from api
    
    * fix schedule and section types
    
    * move enums above interfaces
    
    Co-authored-by: Davis Haupt <davis.haupt@gmail.com>

---
+++ frontend/plan/types.ts
@@ -0,0 +1,92 @@
+export enum School {
+    SEAS = "SEAS",
+    WHARTON = "WH",
+    COLLEGE = "SAS",
+    NURSING = "NUR",
+}
+
+export enum Status {
+    OPEN = "O",
+    CLOSED = "C",
+    CANCELLED = "X",
+    UNLISTED = "",
+}
+
+export enum Activity {
+    CLINIC = "CLN",
+    DISSERTATION = "DIS",
+    INDEPENDENT_STUDY = "IND",
+    LAB = "LAB",
+    MASTERS_THESIS = "MST",
+    RECITATION = "REC",
+    SEMINAR = "SEM",
+    SENIOR_THESIS = "SRT",
+    STUDIO = "STU",
+    UNDEFINED = "***",
+}
+
+export interface Section {
+    id: string;
+    status: Status;
+    activity: Activity;
+    credits: number;
+    semester: string;
+    meetings: Meeting[];
+    instructors: string[];
+    course_quality: number;
+    instructor_quality: number;
+    difficulty: number;
+    work_required: number;
+    associated_sections: Section[];
+}
+
+export interface Meeting {
+    day: string;
+    start: number;
+    end: number;
+    room: string;
+}
+
+export interface Profile {
+    email: string | null;
+    phone: string | null;
+}
+
+export interface Requirement {
+    id: string;
+    code: string;
+    school: School;
+    semester: string;
+    name: string;
+}
+
+export interface Course {
+    id: string;
+    title: string;
+    sections: Section[];
+    description: string;
+    semester: string;
+    prerequisites: string;
+    course_quality: number;
+    instructor_quality: number;
+    difficulty: number;
+    work_required: number;
+    crosslistings: string[];
+    requirements: Requirement[];
+}
+
+export interface Schedule {
+    id: string;
+    sections: Section[];
+    semester: string;
+    name: string;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface User {
+    username: string;
+    first_name: string;
+    last_name: string;
+    profile: Profile;
+}
