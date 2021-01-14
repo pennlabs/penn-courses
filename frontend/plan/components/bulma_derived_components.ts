085de0e68727424c2bef75b0209449deaac0422d
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2021-01-14 15:43:29 -0500

    extract bulma components (checkbox, radio)

---
+++ frontend/plan/components/bulma_derived_components.ts
@@ -0,0 +1,132 @@
+import styled from "styled-components";
+
+export const Column = styled.div`
+    display: block;
+    flex-basis: 0;
+    flex-grow: 1;
+    flex-shrink: 1;
+    padding: 0.75rem;
+`;
+
+export const RadioInput = styled.input`
+    display: table-cell;
+
+    outline: 0;
+    user-select: none;
+    position: absolute;
+    opacity: 0;
+
+    vertical-align: baseline;
+
+    &:checked + label:after {
+        display: inline-block !important;
+    }
+`;
+
+export const RadioLabel = styled.label`
+    display: table-cell;
+    font-size: 0.75rem;
+    line-height: 1.125rem;
+
+    margin-left: 0;
+
+    position: relative;
+    cursor: pointer;
+    vertical-align: middle;
+    margin: 0.5em;
+    padding: 0.2rem 0.5rem 0.2rem 1.5rem;
+    border-radius: 4px;
+
+    &:before {
+        width: 1.125rem;
+        height: 1.125rem;
+        border-radius: 50%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        content: "";
+        border: 0.1rem solid #dbdbdb;
+    }
+
+    &:hover:before {
+        animation-duration: 0.4s;
+        animation-fill-mode: both;
+        border-color: #00d1b2 !important;
+    }
+
+    &:after {
+        width: 1.125rem;
+        height: 1.125rem;
+
+        display: none;
+
+        border-radius: 50%;
+        background: #00d1b2;
+        left: 0;
+        transform: scale(0.5);
+
+        position: absolute;
+        content: "";
+        top: 0;
+    }
+`;
+
+export const CheckboxInput = styled.input`
+    outline: 0;
+    user-select: none;
+    display: inline-block;
+    position: absolute;
+    opacity: 0;
+    vertical-align: baseline;
+
+    &:checked + label:after {
+        display: inline-block !important;
+    }
+`;
+
+export const CheckboxLabel = styled.label`
+    display: table-cell;
+    position: relative;
+    cursor: pointer;
+    vertical-align: middle;
+    margin: 5em 0.5em 0.5em 0;
+    padding: 0.2rem 0.5rem 0.2rem 0;
+    border-radius: 4px;
+    font-size: 0.75rem;
+    padding-left: 1.5rem;
+
+    &:before {
+        width: 1.125rem;
+        height: 1.125rem;
+        position: absolute;
+        left: 0;
+        top: 0;
+        content: "";
+        border: 0.1rem solid #dbdbdb;
+        border-radius: 4px;
+    }
+
+    &:hover:before {
+        animation-duration: 0.4s;
+        animation-fill-mode: both;
+        border-color: #00d1b2 !important;
+    }
+
+    &:after {
+        width: 0.28125rem;
+        height: 0.45rem;
+        top: 0.30375rem;
+        left: 0.45rem;
+
+        display: none;
+
+        box-sizing: border-box;
+        transform: translateY(0) rotate(45deg);
+        border: 0.1rem solid #00d1b2;
+        border-top: 0;
+        border-left: 0;
+
+        position: absolute;
+        content: "";
+    }
+`;
--- frontend/plan/components/search/CheckboxFilter.tsx
+++ frontend/plan/components/search/CheckboxFilter.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { CheckboxInput, CheckboxLabel } from "../bulma_derived_components";
 
 type CheckboxFilterData<D> = {
     [K in keyof D]: boolean;
@@ -50,66 +51,6 @@ const FilterField = styled.div`
     display: table-row;
 `;
 
-const FilterInput = styled.input`
-    outline: 0;
-    user-select: none;
-    display: inline-block;
-    position: absolute;
-    opacity: 0;
-    vertical-align: baseline;
-
-    &:checked + label:after {
-        display: inline-block !important;
-    }
-`;
-
-const FilterLabel = styled.label`
-    display: table-cell;
-    position: relative;
-    cursor: pointer;
-    vertical-align: middle;
-    margin: 0.5em 0.5em 0.5em 0;
-    padding: 0.2rem 0.5rem 0.2rem 0;
-    border-radius: 4px;
-    font-size: 0.75rem;
-    padding-left: 1.5rem;
-
-    &:before {
-        width: 1.125rem;
-        height: 1.125rem;
-        position: absolute;
-        left: 0;
-        top: 0;
-        content: "";
-        border: 0.1rem solid #dbdbdb;
-        border-radius: 4px;
-    }
-
-    &:hover:before {
-        animation-duration: 0.4s;
-        animation-fill-mode: both;
-        border-color: #00d1b2 !important;
-    }
-
-    &:after {
-        width: 0.28125rem;
-        height: 0.45rem;
-        top: 0.30375rem;
-        left: 0.45rem;
-
-        display: none;
-
-        box-sizing: border-box;
-        transform: translateY(0) rotate(45deg);
-        border: 0.1rem solid #00d1b2;
-        border-top: 0;
-        border-left: 0;
-
-        position: absolute;
-        content: "";
-    }
-`;
-
 // mapped types
 
 export function CheckboxFilter<
@@ -134,7 +75,7 @@ export function CheckboxFilter<
                     return (
                         <FilterColumn key={filterProperty}>
                             <FilterField>
-                                <FilterInput
+                                <CheckboxInput
                                     type="checkbox"
                                     id={filterProperty}
                                     checked={
@@ -162,9 +103,9 @@ export function CheckboxFilter<
                                     }}
                                 />
                                 {/* eslint-disable-next-line jsx-a11y/label-has-for */}
-                                <FilterLabel htmlFor={filterProperty}>
+                                <CheckboxLabel htmlFor={filterProperty}>
                                     {filterProperty}
-                                </FilterLabel>
+                                </CheckboxLabel>
                             </FilterField>
                         </FilterColumn>
                     );
