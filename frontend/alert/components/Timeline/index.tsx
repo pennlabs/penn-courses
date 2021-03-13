955130c86840b0163f1ba92eb92612d3d3c9abcb
andy jiang <andyjiang55@yahoo.com>
2021-03-13 16:05:54 -0500

    useRef is unused

--- frontend/alert/components/Timeline/index.tsx
+++ frontend/alert/components/Timeline/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
