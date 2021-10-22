7dfc2bd89a1312bcb6097d11f891857ff9279b55
Charley Cunningham <mureytasroc@gmail.com>
2021-10-22 18:18:10 -0400

    Deduplicate Status Updates

---
+++ backend/courses/management/commands/deduplicate_status_updates.py
@@ -0,0 +1,29 @@
+from textwrap import dedent
+
+from django.core.management.base import BaseCommand
+from alert.management.commands.recomputestats import deduplicate_status_updates
+
+
+class Command(BaseCommand):
+    help = "Remove duplicate/redundant status updates from the given semesters."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--semesters",
+            type=str,
+            help=dedent(
+                """
+                The semesters argument should be a comma-separated list of semesters
+            corresponding to the semesters for which you want to remove duplicate/redundant
+            status updates, i.e. "2019C,2020A,2020C" for fall 2019, spring 2020, and fall 2020.
+            If this argument is omitted, stats are only recomputed for the current semester.
+            If you pass "all" to this argument, this script will remove duplicate/redundant
+            status updates for all semesters found in Courses in the db.
+                """
+            ),
+            nargs="?",
+            default=None,
+        )
+
+    def handle(self, *args, **kwargs):
+        deduplicate_status_updates(semesters=kwargs["semesters"], verbose=True)
