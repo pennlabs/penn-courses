08369ced87abca476275fcdd528dad7dc92ceb49
Charles Cunningham <mureytasroc@gmail.com>
2022-03-19 23:20:47 -0400

    Change dev PCA_URL

--- backend/PennCourses/settings/development.py
+++ backend/PennCourses/settings/development.py
@@ -3,7 +3,7 @@ import os
 from PennCourses.settings.base import *  # noqa: F401, F403
 
 
-PCA_URL = "http://localhost:8000"
+PCA_URL = "http://127.0.0.1:8000"
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
