9ee5817c4c480163e079dea7ad126ff6773aa708
Charles Cunningham <mureytasroc@gmail.com>
2022-03-19 21:41:46 -0400

    Catch IntegrityErrors in ISC import script

--- backend/review/import_utils/import_to_db.py
+++ backend/review/import_utils/import_to_db.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 from tqdm import tqdm
 
 from courses.models import Course, Instructor
@@ -60,7 +61,11 @@ def import_instructor(pennid, fullname, stat):
             inst.save()
             inst_created = False
         else:
-            inst = Instructor.objects.create(user=user, name=fullname)
+            try:
+                inst = Instructor.objects.create(user=user, name=fullname)
+            except IntegrityError:
+                stat("instructor:integrity_error")
+                return
             inst_created = True
 
         if inst_created:
@@ -116,22 +121,30 @@ def import_course_and_section(full_course_code, semester, course_title, primary_
 def import_review(section, instructor, enrollment, responses, form_type, bits, stat):
     # Assumption: that all review objects for the semesters in question were
     # deleted before this runs.
-    review, created = Review.objects.get_or_create(
-        section=section,
-        instructor=instructor,
-        defaults={
-            "enrollment": enrollment,
-            "responses": responses,
-            "form_type": form_type,
-        },
-    )
-    if not created:
-        stat("duplicate_review")
+    try:
+        review, created = Review.objects.get_or_create(
+            section=section,
+            instructor=instructor,
+            defaults={
+                "enrollment": enrollment,
+                "responses": responses,
+                "form_type": form_type,
+            },
+        )
+        if not created:
+            stat("duplicate_review")
+    except IntegrityError:
+        stat("review:integrity_error")
+        return
     review_bits = [ReviewBit(review=review, field=k, average=v) for k, v in bits.items()]
 
     # This saves us a bunch of database calls per row, since reviews have > 10 bits.
-    ReviewBit.objects.bulk_create(review_bits, ignore_conflicts=True)
-    stat("reviewbit_created_count", len(review_bits))
+    try:
+        ReviewBit.objects.bulk_create(review_bits, ignore_conflicts=True)
+        stat("reviewbit_created_count", len(review_bits))
+    except IntegrityError:
+        stat("review:integrity_error")
+        return
 
 
 def import_summary_row(row, stat):
