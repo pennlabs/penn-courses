cab274a005e08d0879ce70ebeaf9bc6f925267e5
AaDalal <57609353+AaDalal@users.noreply.github.com>
2022-06-14 00:38:04 -0400

    Linting

--- backend/courses/migrations/0052_merge_20220614_0027.py
+++ backend/courses/migrations/0052_merge_20220614_0027.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('courses', '0051_alter_attribute_school'),
-        ('courses', '0051_alter_ngssrestriction_inclusive'),
+        ("courses", "0051_alter_attribute_school"),
+        ("courses", "0051_alter_ngssrestriction_inclusive"),
     ]
 
-    operations = [
-    ]
+    operations = []
