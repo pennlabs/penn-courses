a5a8c2c7f9fb02e4d02a5068009c6215b4b57d7a
Ansh Nagwekar <62483645+anshnagwekar@users.noreply.github.com>
2022-09-13 09:30:53 -0400

    url paths for shared schedules

--- backend/plan/urls.py
+++ backend/plan/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 from rest_framework import routers
 
-from plan.views import ScheduleViewSet, recommend_courses_view
+from plan.views import ScheduleViewSet, recommend_courses_view, get_shared_schedule, set_shared_schedule
 
 
 router = routers.DefaultRouter()
@@ -12,5 +12,7 @@ router.register(r"schedules", ScheduleViewSet, basename="schedules")
 urlpatterns = [
     path("", TemplateView.as_view(template_name="plan/build/index.html")),
     path("recommendations/", recommend_courses_view, name="recommend-courses"),
+    path("shared/get_schedule", get_shared_schedule, name="get-shared-schedule"),
+    path("shared/set_schedule", set_shared_schedule, name="set-shared-schedule"),
     path("", include(router.urls)),
 ]
