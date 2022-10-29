9c7d100381bfcbe74c362d57cfe0e224aea86f75
AaDalal <57609353+AaDalal@users.noreply.github.com>
2022-10-29 02:58:40 -0400

    Debug script; lint

--- backend/courses/management/commands/fill_meetings_from_section_meeting_times.py
+++ backend/courses/management/commands/fill_meetings_from_section_meeting_times.py
@@ -1,10 +1,11 @@
 import json
 import logging
 
-import tqdm
 from django.core.management.base import BaseCommand
 from django.db import transaction
+from tqdm import tqdm
 
+from alert.management.commands.recomputestats import recompute_stats
 from courses.models import Meeting, Section
 
 
@@ -33,7 +34,9 @@ def convert_time(time: str, time_period: str):
 
 def delete_prompt(section: Section) -> bool:
     while True:
-        inp = input(f"delete meetings {[str(meeting) for meeting in section.meetings]}? [y/n]")
+        inp = input(
+            f"delete meetings {[str(meeting) for meeting in section.meetings.all()]}? [y/n]"
+        )
         if inp == "y":
             return True
         elif inp == "n":
@@ -44,17 +47,17 @@ class Command(BaseCommand):
     help = "Create meetings from the meeting_times strings of sections"
 
     def add_arguments(self, parser):
-        parser.add_arguments(
+        parser.add_argument(
             "--force",
             action="store_true",
             help="Do not prompt when meetings already exist for a section",
         )
 
-        parser.add_arguments(
+        parser.add_argument(
             "--fail_loud", action="store_true", help="Fail loudly if a meeting_times fails to parse"
         )
 
-        parser.add_arguments(
+        parser.add_argument(
             "--dry_run", action="store_true", help="Dry run, without actually touching database"
         )
 
@@ -86,3 +89,5 @@ class Command(BaseCommand):
                     continue
                 for meeting in meetings:
                     meeting.save()
+        print("Running recompute_stats (to makes sure Section.num_meetings is set correctly)...")
+        recompute_stats()
