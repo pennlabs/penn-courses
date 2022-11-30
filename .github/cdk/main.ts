68134ee23e114d30f2be1cf0bed8bece087605ae
Joy Liu <joyliu.q@gmail.com>
2022-11-29 22:59:32 -0500

    :fire: Nuke CI/CD

--- .github/cdk/main.ts
+++ .github/cdk/main.ts
@@ -1,71 +1,45 @@
 import { Construct } from "constructs";
-import { App, Stack, Workflow } from "cdkactions";
-import { DeployJob, DjangoProject, DockerPublishJob, ReactProject } from "@pennlabs/kraken";
+import { App, Stack } from "cdkactions";
+// import { DeployJob, DjangoProject, DockerPublishJob, ReactProject } from "@pennlabs/kraken";
 
 export class MyStack extends Stack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
     // define workflows here
-    const workflow = new Workflow(this, "workflow", {
-      name: "Workflow",
-      on: "push",
-    });
+    // const workflow = new Workflow(this, "workflow", {
+    //   name: "Workflow",
+    //   on: "push",
+    // });
 
-    const django = new DjangoProject(workflow, {
-      projectName: "PennCourses",
-      path: "backend",
-      imageName: "penn-courses-backend",
-      checkProps: {
-        pythonVersion: "3.10-buster",
-      }
-    });
+    // const django = new DjangoProject(workflow, {
+    //   projectName: "PennCourses-degree",
+    //   path: "backend",
+    //   imageName: "penn-courses-backend",
+    //   checkProps: {
+    //     pythonVersion: "3.10-buster",
+    //   }
+    // });
 
-    const plan = new ReactProject(workflow, {
-      id: "pcp",
-      path: "frontend",
-      imageName: "pcp-frontend",
-      publishProps: {
-        dockerfile: "plan/Dockerfile",
-      },
-    });
+    // const degree = new ReactProject(workflow, {
+    //   id: "pdp",
+    //   path: "frontend",
+    //   imageName: "pdp-frontend",
+    //   publishProps: {
+    //     dockerfile: "degree/Dockerfile",
+    //   },
+    // });
 
-    const alert = new ReactProject(workflow, {
-      id: "pca",
-      path: "frontend",
-      imageName: "pca-frontend",
-      publishProps: {
-        dockerfile: "alert/Dockerfile",
-      },
-    });
-
-    const review = new ReactProject(workflow, {
-      id: "pcr",
-      path: "frontend",
-      imageName: "pcr-frontend",
-      publishProps: {
-        dockerfile: "review/Dockerfile",
-      },
-    });
-
-    const landing = new DockerPublishJob(workflow, 'landing', {
-      imageName: 'pcx-landing',
-      path: 'frontend/landing',
-    });
-
-    new DeployJob(
-      workflow,
-      {},
-      {
-        needs: [
-          django.publishJobId,
-          plan.publishJobId,
-          alert.publishJobId,
-          review.publishJobId,
-          landing.id,
-        ],
-      }
-    );
+    // new DeployJob(
+    //   workflow,
+    //   {},
+    //   {
+    //     needs: [
+    //       django.publishJobId,
+    //       degree.publishJobId,
+    //     ],
+    //   }
+    // );
   }
 }
 
