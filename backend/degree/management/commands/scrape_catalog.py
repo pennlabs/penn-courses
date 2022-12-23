ba0e7e7da69cdee88feb4713a86ebcb7ced24992
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2022-12-22 19:56:38 -0500

    Mostly fix web page -> AST scraping

--- backend/degree/management/commands/scrape_catalog.py
+++ backend/degree/management/commands/scrape_catalog.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 import requests
 import re
 from bs4 import BeautifulSoup
@@ -18,8 +20,9 @@ class CourseLeaf(RequirementNode):
         self.credits = credits
 
     def __str__(self):
-        return "COURSE: %s - %s" % self.full_code, self.credits
+        return f"COURSE: {self.full_code} - {self.credits}"
 
+    @property
     def children(self):
         return []
 
@@ -36,13 +39,14 @@ class CourseSetLeaf(RequirementNode):
             num_str = "%s courses" % self.num_courses
         else:
             num_str = "%s credits" % self.num_credits
-        return "COURSE_SET: %s - %s" % self.course_set, num_str
+        return f"COURSE_SET: {self.course_set} - {num_str}"
 
+    @property
     def children(self) -> [RequirementNode]:
         return []
 
 
-class LabelNode(RequirementNode):
+class BranchingNode(RequirementNode):
     def __init__(self, label: str, children=None, num_courses: None | str = None, num_credits: None | str = None):
         if children is None:
             children = []
@@ -52,34 +56,31 @@ class LabelNode(RequirementNode):
         self.num_credits = num_credits
 
     def __str__(self):
-        return "LABEL: %s" % self.label
+        if self.num_courses:
+            num_str = "%s courses" % self.num_courses
+        else:
+            num_str = "%s credits" % self.num_credits
+        return f"`f{self.label}` {num_str}"
 
+    @property
     def children(self):
         return self.children_
 
 
-class OrNode(LabelNode):
+class OrNode(BranchingNode):
     def __str__(self):
-        if self.num_courses:
-            num_str = "%s courses" % self.num_courses
-        else:
-            num_str = "%s credits" % self.num_credits
-        return "OR: %s" % num_str
+        return "OR: " + super().__str__()
 
-
-class AndNode(LabelNode):
+class AndNode(BranchingNode):
     def __str__(self):
-        if self.num_courses:
-            num_str = "%s courses" % self.num_courses
-        else:
-            num_str = "%s credits" % self.num_credits
-        return "AND: %s" % num_str
+        return "AND: " + super().__str__()
 
 
 class NotNode(RequirementNode):
     def __init__(self, child: RequirementNode):
         self.child = child
 
+    @property
     def children(self):
         return [self.child]
 
@@ -100,15 +101,18 @@ def get_course_units(is_course_units, num):
 
 
 def scrape(soup):
-    re_header_class = re.compile("area?(sub)?(sub)?(sub)?(sub)?(sub)?(sub)?(sub)?(sub)header") # TODO: hacky way of capturing up to 7 layers of "subs"
-    re_select_n = \
-        "Select \b(one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|" \
-        "thirteen|fourteen|fifteen|sixteen|seventeen|eighteen|nineteen|twenty|[0-9]+)\b"
-    re_select_following = re.compile(re_select_n + " ?(course units of) the following (.*):")
-    re_select_course_set = re.compile(re_select_n + " ?(course units of) (.*) courses")
+    re_header_class = re.compile(
+        "area(sub)?(sub)?(sub)?(sub)?(sub)?(sub)?(sub)?(sub)?header")  # TODO: hacky way of capturing up to 7 layers of "subs"
+    re_select_n = ("Select (one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen|fourteen|fifteen"
+                   "|sixteen|seventeen|eighteen|nineteen|twenty|[0-9]+) ")
+    re_select_following = re.compile(re_select_n + "(course units )?of the following ?(.*)")
+    re_select_course_set = re.compile(re_select_n + "(course units of |course units )?(.*) courses")
+
+    # Degree name
+    degree_name = soup.find("h1", class_="page-title").text
 
     # Help traversal up the tree
-    stack = [AndNode("[Degree Name]")]
+    stack = [AndNode(f"{degree_name}")]  # TODO
     breadcrumbs = [0]  # indices within stack where currently applicable headings & subheadings live
 
     soup.prettify(formatter=lambda s: s.replace(u'\xa0', ' '))  # replace &nbsp with regular spaces
@@ -117,41 +121,41 @@ def scrape(soup):
     for row in table.find_all("tr"):
         parent = stack[-1]
         obj = None
-        print("--")
-        print(row.text)
-        print(stack)
-        print(breadcrumbs)
         if row.has_attr("class") and (match := re.search(re_header_class, " ".join(row['class']))) is not None:
             heading_depth = len([group for group in match.groups() if group is not None])
-            print("match.string:", match.string)
-            print(f"heading_depth: {heading_depth}")
-            obj = LabelNode(row.text)
-            parent = breadcrumbs[heading_depth]
+            obj = AndNode(row.text)
+            parent = stack[breadcrumbs[heading_depth]]
             stack[(breadcrumbs[heading_depth] + 1):] = []  # pop stack past parent
             breadcrumbs[(heading_depth + 1):] = [len(stack)]  # set breadcrumbs
             stack.append(obj)
         elif hours := row.find("td", class_="hourscol"):
-            if comment := row.find("td", class_="courselistcomment"):
+            if comment := row.find(class_="courselistcomment"):
                 if match := re.search(re_select_following, comment.text):
                     is_course_units = match.group(2)
-                    num = match.group(1)
+                    num = match.group(1)  # Could be None
                     obj = OrNode(comment.text, **get_course_units(is_course_units, num))
                     stack.append(obj)
                 elif match := re.search(re_select_course_set, comment.text):
                     is_course_units = match.group(2)
                     num = match.group(1)
                     obj = CourseSetLeaf(match.group(3), **get_course_units(is_course_units, num))
+                elif hours.text.strip(): # TODO: should this also be a course set?
+                    obj = CourseSetLeaf(comment.text, num_credits=hours.text, num_courses=None)
+
             elif full_code := row.find("td", class_="codecol"):
                 obj = CourseLeaf(
                     full_code.text.strip().upper().replace(" ", "-"),
                     hours.text
                 )
-        parent.children_.append(obj)
+        if obj is None:
+            print(f"`{row.text}` could not be identified")
+        else:
+            parent.children_.append(obj)
     return stack[0]
 
 
 class Command(BaseCommand):
-    help = "This script scrapes a provided Penn Catalog (catalog.upenn.edu) url into JSON."
+    help = "This script scrapes a provided Penn Catalog (catalog.upenn.edu) into an AST"
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -164,17 +168,24 @@ class Command(BaseCommand):
         )
         parser.add_argument(
             "--response-dump-file",
-            help="File to dump response from URL to",
+            nargs="?",
+            const=True,
+            help=dedent(
+                """
+            File to dump response from URL to. If the path is not provided, it is inferred from the last part of the URL
+            and placed in in the ./degree/data directory of (where "." is the current working directory).
+            """
+            ),
         )
 
     def handle(self, *args, **kwargs):
-        print(kwargs)
         if (url := kwargs.get("url")) is not None:
             page = requests.get(url).text
             if (out_file_path := kwargs.get("response_dump_file")) is not None:
+                if out_file_path == True: # check if provided as a flag
+                    out_file_path = "./degree/data/" + [piece for piece in url.rsplit("/", 2) if piece.strip() != ''][-1] + ".html"
                 with open(out_file_path, "w") as f:
                     f.write(page)
-                print(out_file_path)
         elif (in_file_path := kwargs.get("file")) is not None:
             page = open(in_file_path)
         else:
