727e56dbaf96dc14f62916527405777926b316ab
Raunaq Singh <raunaqsingh2020@gmail.com>
2022-12-27 10:53:48 -0500

    fix url

--- backend/plan/urls.py
+++ backend/plan/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 from rest_framework import routers
 
-from plan.views import ScheduleViewSet, recommend_courses_view, recommend_schedules_view
+from plan.views import ScheduleViewSet, recommend_courses_view, recommend_schedule_view
 
 
 router = routers.DefaultRouter()
@@ -11,7 +11,7 @@ router.register(r"schedules", ScheduleViewSet, basename="schedules")
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="plan/build/index.html")),
-    path("recommendations/schedules/", recommend_schedules_view, name="recommend-schedule"),
+    path("recommendations/schedule/", recommend_schedule_view, name="recommend-schedule"),
     path("recommendations/courses/", recommend_courses_view, name="recommend-courses"),
     path("", include(router.urls)),
 ]
