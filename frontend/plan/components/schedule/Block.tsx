4c8737ed6470e300735823792506f98b153ee630
Andy Jiang <andyjiang55@yahoo.com>
2023-03-26 14:09:10 -0400

    increase font

--- frontend/plan/components/schedule/Block.tsx
+++ frontend/plan/components/schedule/Block.tsx
@@ -86,7 +86,7 @@ const InnerBlock = styled.div`
     flex-direction: column;
     width: 100%;
     height: 100%;
-    font-size: 0.85em;
+    font-size: 0.9em;
 
     & > span {
         cursor: pointer;
