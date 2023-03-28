19269955fb5ef468ac6f95cd2970f436ee419b3a
Alexander Kyimpopkin <39439486+alxkp@users.noreply.github.com>
2023-03-28 13:47:41 -0400

    updated redisapplication imagename

--- k8s/main.ts
+++ k8s/main.ts
@@ -11,7 +11,9 @@ export class MyChart extends PennLabsChart {
     const backendImage = 'pennlabs/penn-courses-backend';
     const secret = 'penn-courses';
 
-    new RedisApplication(this, 'redis/redis-stack', { deployment: { tag: '6.2.6-v6' } });
+    new RedisApplication(this, 'redis-stack', { deployment: { 
+	    image: 'redis/redis-stack-server',
+	    tag: '6.2.6-v6' } });
 
     new DjangoApplication(this, 'celery', {
       deployment: {
