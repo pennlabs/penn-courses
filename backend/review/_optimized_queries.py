b5408f7205f0d2a834d525d26d8372b083f92af5
AaDalal <57609353+AaDalal@users.noreply.github.com>
2023-08-14 11:41:13 -0400

    In progress query optimizations

---
+++ backend/review/_optimized_queries.py
@@ -0,0 +1,4 @@
+qs = ReviewBit.objects.filter(
+    reviewbit_filters_pcr & Q(review__instructor__id=1),
+    review__responses__gt=0,
+).values("field").annotate(avg=Avg("average", output_field=FloatField())).order_by()
