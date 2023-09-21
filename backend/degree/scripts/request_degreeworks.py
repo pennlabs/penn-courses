7b7fd802f1e00ff1b9d35a10a67adda5832738ad
Christopher Liu <liuchris@seas.upenn.edu>
2023-09-21 13:57:24 -0400

    Include degree, year, and concentration in degreeworks call

--- backend/degree/scripts/request_degreeworks.py
+++ backend/degree/scripts/request_degreeworks.py
@@ -29,10 +29,11 @@ def audit(degree_plan: DegreePlan, timeout=30):
         "isIncludePreregistered": True,
         "isKeepCurriculum": False,
         "school": "UG",
-        "degree": "BA",
-        "catalogYear": "2023",
+        "degree": degree_plan.degree,
+        "catalogYear": degree_plan.year,
         "goals": [
             {"code": "MAJOR", "value": degree_plan.major},
+            {"code": "CONC", "value": degree_plan.concentration},
             {"code": "PROGRAM", "value": degree_plan.program},
             {"code": "COLLEGE", "value": degree_plan.program.split("_")[0]},
         ],
