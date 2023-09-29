d306db5aaaa8eb4886ef9ab1613d693aef4dd6fc
Yuntong Fu <yuntongf@sas.upenn.edu>
2023-09-29 10:27:04 -0400

    try to merge

--- frontend/degree-plan/components/FourYearPlan/CoursesPlanned.tsx
+++ frontend/degree-plan/components/FourYearPlan/CoursesPlanned.tsx
@@ -1,13 +1,14 @@
 
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CoursePlanned from "./CoursePlanned";
 
 const CoursesPlanned = ({courses, semesterIndex, removeCourse}: any) => {
+    const [courseOpen, setCourseOpen] = useState("");
     return (
         <div className="">
             {courses.map((course: any) => 
-                <CoursePlanned course={course} semesterIndex={semesterIndex} removeCourse={removeCourse}/>
+                <CoursePlanned course={course} semesterIndex={semesterIndex} removeCourse={removeCourse} courseOpen={courseOpen} setCourseOpen={setCourseOpen}/>
             )}
         </div>
     )
