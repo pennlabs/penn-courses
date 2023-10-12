6795d33f05edb1d92dbf41bd77b24ee12df92d52
AaDalal <57609353+AaDalal@users.noreply.github.com>
2023-10-12 00:01:28 -0400

    test + fix q object parser

--- backend/degree/utils/model_utils.py
+++ backend/degree/utils/model_utils.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 from lark import Lark, Transformer
 from functools import reduce
 from operator import or_, and_
+from ast import literal_eval
 
 """
 This file contains a parser for that can convert a stringified Q object
@@ -9,6 +10,9 @@ This file contains a parser for that can convert a stringified Q object
 relies on the stability of the Q object __repr__ method. Run the associated
 tests to make sure the output of Q(...).__repr__ is as expected and that this
 parser re-builds the Q object correctly.
+
+Note that when you manually test this parser against strings, you'll want to use
+raw strings (ie, q_object_parser.parse(r"..."))
 """
 
 class QObjectTransformer(Transformer):
@@ -23,16 +27,14 @@ class QObjectTransformer(Transformer):
         """
         return list(n)
 
-    def and_clause(self, n):
-        clauses, = n
-        return reduce(and_, clauses)
+    def and_clause(self, clauses):
+        return reduce(and_, clauses, Q())
     
-    def or_clause(self, n):
-        clauses, = n
-        return reduce(or_, clauses)
+    def or_clause(self, clauses):
+        return reduce(or_, clauses, Q())
     
-    def not_clause(self, n):
-        clause, = n
+    def not_clause(self, clauses):
+        clause, = clauses
         return ~clause
     
     def condition(self, n):
@@ -41,7 +43,7 @@ class QObjectTransformer(Transformer):
 
     def string(self, s):
         (s,) = s
-        return s.strip()
+        return bytes(s[1:-1], 'utf-8').decode("unicode_escape") # strip off quotes
 
     def sint(self, n):
         (n,) = n
@@ -67,24 +69,28 @@ q_object_parser = Lark(
            | or_clause
            | not_clause
           // NOTE: XOR clauses are not supported (yet...)
+
+    ?connector_clause: and_clause
+                    | or_clause
      
     condition: "(" string "," value ")"
-    and_clause: "(AND:" [clause ("," clause)*] ")"
+    and_clause:"(AND:" [clause ("," clause)*] ")"
     or_clause: "(OR:" [clause ("," clause)*] ")"
-    not_clause: "(NOT" clause ")"
+    not_clause: "(NOT" connector_clause ")"
 
-    ?value: string 
+    ?value: string
           | SIGNED_INT -> sint
           | SIGNED_FLOAT -> sfloat
           | none
     
     none: "None"
-    string: /'([^']|(\'))*'/
-          | /"([^']|(\"))*"/
+    string: /'.*?(?<!\\)(\\\\)*?'/ // escapable strings
+          | /".*?(?<!\\)(\\\\)*?"/
 
     %import common.SIGNED_INT
     %import common.SIGNED_FLOAT
     %import common.WS
+    %import common._STRING_ESC_INNER
     %ignore WS
     """,
     start="q",
--- backend/tests/degree/test_models.py
+++ backend/tests/degree/test_models.py
@@ -1,16 +1,19 @@
 from django.test import TestCase
 from degree.utils.model_utils import q_object_parser
 from django.db.models import Q
-from lark.exceptions import ParseError
+from lark.exceptions import LarkError
 
 class QObjectParserTest(TestCase):
     def assertParsedEqual(self, q: Q):
-        print(repr(q))
         self.assertEqual(q_object_parser.parse(repr(q)), q)
         self.assertEqual(
             repr(q_object_parser.parse(repr(q))), 
             repr(q)
         )
+
+    def test_no_clause(self):
+        with self.assertRaises(LarkError): 
+            q_object_parser.parse(r"<Q: >")
 
     def test_empty_q(self):
         self.assertParsedEqual(Q())
@@ -19,22 +22,22 @@ class QObjectParserTest(TestCase):
         self.assertParsedEqual(~Q())
 
     def test_string_condition(self):
-        self.assertParsedEquals(Q(key="value"))
+        self.assertParsedEqual(Q(key="value"))
     
     def test_null_condition(self):
-        self.assertParsedEquals(Q(key=None))
+        self.assertParsedEqual(Q(key=None))
     
     def test_int_condition(self):
-        self.assertParsedEquals(Q(key=100))
+        self.assertParsedEqual(Q(key=100))
     
     def test_signed_int_condition(self):
-        self.assertParsedEquals(Q(key=-100))
+        self.assertParsedEqual(Q(key=-100))
 
     def test_float_condition(self):
-        self.assertParsedEquals(Q(key=1.0))
+        self.assertParsedEqual(Q(key=1.0))
     
     def test_signed_float_condition(self):
-        self.assertParsedEquals(Q(key=-1.0))    
+        self.assertParsedEqual(Q(key=-1.0))    
 
     def test_string_escaping(self):
         self.assertParsedEqual(Q(key="\""))
@@ -43,7 +46,7 @@ class QObjectParserTest(TestCase):
 
 
     def test_and(self):
-        self.assertParsedEqual(Q(key="value") & Q(key2="value2"))
+        self.assertParsedEqual(Q(key=r"value") & Q(key2=r"value2"))
 
     def test_or(self):
         self.assertParsedEqual(Q(key="value") | Q(key2="value2"))
@@ -62,13 +65,15 @@ class QObjectParserTest(TestCase):
         self.assertParsedEqual(~~Q(key="value"))
 
     def test_unparseable_clause(self):
-        with self.assertRaises(ParseError):
-            q_object_parser.parse("<Q: (XOR: ('key', 'value'))>")
+        with self.assertRaises(LarkError):
+            q_object_parser.parse(r"<Q: (XOR: ('key', 'value'))>")
 
     def test_unparseable_value(self):
-        with self.assertRaises(ParseError):
-            q_object_parser.parse("<Q: (AND: ('key', datetime.datetime(2023, 10, 11, 1, 24, 6, 114278)))>")
+        with self.assertRaises(LarkError):
+            q_object_parser.parse(r"<Q: (AND: ('key', datetime.datetime(2023, 10, 11, 1, 24, 6, 114278)))>")
 
+    def test_idempotency(self):
+        self.assertParsedEqual(q_object_parser.parse(repr(Q(key="\"'value"))))
     
         
 
