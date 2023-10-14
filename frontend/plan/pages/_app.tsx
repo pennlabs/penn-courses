bdf3ce74e5febe759e6247f6d53866eb6d1fbf2e
Eunsoo Shin (esinx) <me@esinx.net>
2023-10-14 17:27:34 -0400

    feat(temporary banners)

--- frontend/plan/pages/_app.tsx
+++ frontend/plan/pages/_app.tsx
@@ -8,6 +8,7 @@ import "rc-slider/assets/index.css";
 import "../styles/App.css";
 import * as Sentry from "@sentry/browser";
 import type { AppProps } from "next/app";
+import { Announcement } from "pcx-shared-components/src/common/Announcement";
 
 Sentry.init({
     dsn: "https://b476d74f4a224b5ea5bd44449cfc5d67@sentry.pennlabs.org/17",
@@ -15,7 +16,23 @@ Sentry.init({
 
 // eslint-disable-next-line
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <div
+                style={{
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    padding: 20,
+                    zIndex: 1000,
+                }}
+            >
+                <Announcement type="issue" title="Weekend Maintenance Alert" />
+            </div>
+        </>
+    );
 }
 
 export default App;
