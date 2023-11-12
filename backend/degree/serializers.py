edcbc498fdaf32e51effcfb15e997033aba05c72
Christopher Liu <liuchris@seas.upenn.edu>
2023-11-12 13:47:54 -0500

    Add degree list and degree detail views

--- backend/degree/serializers.py
+++ backend/degree/serializers.py
@@ -7,18 +7,27 @@ from degree.models import DegreePlan, Rule
 
 
 class RuleSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Rule
-        fields = '__all__'
+        fields = "__all__"
+
+
+RuleSerializer._declared_fields["rules"] = RuleSerializer(
+    many=True, read_only=True, source="children"
+)
 
 
 class DegreePlanSerializer(serializers.ModelSerializer):
-    
+    class Meta:
+        model = DegreePlan
+        fields = "__all__"
+
+
+class DegreePlanDetailSerializer(serializers.ModelSerializer):
+
     # field to represent the rules related to this Degree Plan
-    rules = RuleSerializer(many=True, read_only=True)
+    rules = RuleSerializer(many=True, read_only=True, source="rule_set")
 
     class Meta:
         model = DegreePlan
-        fields = '__all__'
-
+        fields = "__all__"
