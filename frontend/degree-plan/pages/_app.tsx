beb140cbd792c7f29aa293288478a54e829ecdf2
August Fu <omgitsmonday@nic-10467.apn.wlan.private.upenn.edu>
2023-11-25 11:48:55 -0500

    remove react-redux

--- frontend/degree-plan/pages/_app.tsx
+++ frontend/degree-plan/pages/_app.tsx
@@ -1,11 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import React from 'react';
-import { wrapper } from "../store/configureStore";
 
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default wrapper.withRedux(App)
+export default App
