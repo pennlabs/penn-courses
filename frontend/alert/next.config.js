d925e700af888b6ca535d5ea16e64d2f304dd048
aadalal <57609353+AaDalal@users.noreply.github.com>
2024-02-09 20:58:18 -0500

    switch from babel to swc for next.js products

--- frontend/alert/next.config.js
+++ frontend/alert/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['pcx-shared-components']
+  transpilePackages: ['pcx-shared-components'],
+  compiler: {
+    styledComponents: true
+  }
 };
 
 module.exports = nextConfig;
