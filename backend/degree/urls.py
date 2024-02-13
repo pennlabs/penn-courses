c0441100f229553a2ccf77ea87f272ffddab6d5d
aadalal <57609353+AaDalal@users.noreply.github.com>
2024-02-13 11:49:38 -0500

    fix trailing slash issues with nextjs

--- backend/degree/urls.py
+++ backend/degree/urls.py
@@ -5,7 +5,7 @@ from rest_framework_nested.routers import NestedDefaultRouter
 from degree.views import DegreePlanViewset, DegreeViewset, FulfillmentViewSet, courses_for_rule
 
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r"degreeplans", DegreePlanViewset, basename="degreeplan")
 router.register(r"degrees", DegreeViewset, basename="degree")
 fulfillments_router = NestedDefaultRouter(router, r"degreeplans", lookup="degreeplan")
