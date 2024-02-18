f94c22cb4471df7180feaedadc2ab3f6dd53ee60
zhouhelena <helenazhou04@gmail.com>
2024-02-18 13:52:14 -0500

    init

--- frontend/degree-plan/pages/index.tsx
+++ frontend/degree-plan/pages/index.tsx
@@ -1,13 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import 'react-toastify/dist/ReactToastify.css'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import Nav from '../components/NavBar/Nav'
-import FourYearPlanPage from './FourYearPlanPage';
+import "bootstrap/dist/css/bootstrap.css";
+import "react-toastify/dist/ReactToastify.css";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Nav from "../components/NavBar/Nav";
+import FourYearPlanPage from "./FourYearPlanPage";
 import React, { useState } from "react";
-import { type User } from '../types';
-import LoginModal from 'pcx-shared-components/src/accounts/LoginModal';
-import { SWRConfig } from 'swr';
+import { type User } from "../types";
+import LoginModal from "pcx-shared-components/src/accounts/LoginModal";
+import { SWRConfig } from "swr";
 
 export default function Home() {
   const [user, setUser] = useState<User | null>(null);
@@ -16,22 +16,25 @@ export default function Home() {
   const [reqId, setReqId] = useState<undefined | number>(undefined);
 
   const updateUser = (newUserVal: User | null) => {
-      if (!newUserVal) {
-          // the user has logged out; show the login modal
-          setShowLoginModal(true);
-      } else {
-          // the user has logged in; hide the login modal
-          setShowLoginModal(false);
-      }
-      setUser(newUserVal);
+    if (!newUserVal) {
+      // the user has logged out; show the login modal
+      setShowLoginModal(true);
+    } else {
+      // the user has logged in; hide the login modal
+      setShowLoginModal(false);
+    }
+    setUser(newUserVal);
   };
-  
+
   return (
-    <>  
+    <>
       <DndProvider backend={HTML5Backend}>
-        <SWRConfig value={{
-          fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-        }}>
+        <SWRConfig
+          value={{
+            fetcher: (resource, init) =>
+              fetch(resource, init).then((res) => res.json()),
+          }}
+        >
           <Nav
             login={updateUser}
             logout={() => updateUser(null)}
@@ -39,15 +42,20 @@ export default function Home() {
             setSearchClosed={setSearchClosed}
             setReqId={setReqId}
           />
-          <FourYearPlanPage searchClosed={searchClosed} setSearchClosed={setSearchClosed} reqId={reqId} setReqId={setReqId}/>
+          <FourYearPlanPage
+            searchClosed={searchClosed}
+            setSearchClosed={setSearchClosed}
+            reqId={reqId}
+            setReqId={setReqId}
+          />
           {showLoginModal && (
-              <LoginModal
-                  pathname={window.location.pathname}
-                  siteName="Penn Degree Plan"
-              />
+            <LoginModal
+              pathname={window.location.pathname}
+              siteName="Penn Degree Plan"
+            />
           )}
         </SWRConfig>
       </DndProvider>
     </>
-  )
+  );
 }
