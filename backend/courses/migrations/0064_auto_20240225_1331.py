6d46f54869496fea77ec52637e9b0f1238632592
Daniel Zhao <dz7379@gmail.com>
2024-02-25 13:37:33 -0500

    fixed linting for migration

--- backend/courses/migrations/0064_auto_20240225_1331.py
+++ backend/courses/migrations/0064_auto_20240225_1331.py
@@ -6,28 +6,61 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('courses', '0063_auto_20231212_1750'),
+        ("courses", "0063_auto_20231212_1750"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='topic',
-            name='historical_probabilities_fall',
-            field=models.FloatField(default=0, help_text='\nThe historical probability of a student taking a course in this topic in the fall\nsemester, based on historical data. This field is recomputed nightly from the\n`parent_course` graph (in the recompute_soft_state cron job).\n'),
+            model_name="topic",
+            name="historical_probabilities_fall",
+            field=models.FloatField(
+                default=0,
+                help_text="\nThe historical probability of a student taking a course in this topic in the fall\nsemester, based on historical data. This field is recomputed nightly from the\n`parent_course` graph (in the recompute_soft_state cron job).\n",
+            ),
         ),
         migrations.AddField(
-            model_name='topic',
-            name='historical_probabilities_spring',
-            field=models.FloatField(default=0, help_text='\nThe historical probability of a student taking a course in this topic in the spring\nsemester, based on historical data. This field is recomputed nightly from the\n`parent_course` graph (in the recompute_soft_state cron job).\n'),
+            model_name="topic",
+            name="historical_probabilities_spring",
+            field=models.FloatField(
+                default=0,
+                help_text="\nThe historical probability of a student taking a course in this topic in the spring\nsemester, based on historical data. This field is recomputed nightly from the\n`parent_course` graph (in the recompute_soft_state cron job).\n",
+            ),
         ),
         migrations.AddField(
-            model_name='topic',
-            name='historical_probabilities_summer',
-            field=models.FloatField(default=0, help_text='\nThe historical probability of a student taking a course in this topic in the summer\nsemester, based on historical data. This field is recomputed nightly from the\n`parent_course` graph (in the recompute_soft_state cron job).\n'),
+            model_name="topic",
+            name="historical_probabilities_summer",
+            field=models.FloatField(
+                default=0,
+                help_text="\nThe historical probability of a student taking a course in this topic in the summer\nsemester, based on historical data. This field is recomputed nightly from the\n`parent_course` graph (in the recompute_soft_state cron job).\n",
+            ),
         ),
         migrations.AlterField(
-            model_name='section',
-            name='activity',
-            field=models.CharField(choices=[('', 'Undefined'), ('CLN', 'Clinic'), ('CRT', 'Clinical Rotation'), ('DAB', 'Dissertation Abroad'), ('DIS', 'Dissertation'), ('DPC', 'Doctoral Program Exchange'), ('FLD', 'Field Work'), ('HYB', 'Hybrid'), ('IND', 'Independent Study'), ('LAB', 'Lab'), ('LEC', 'Lecture'), ('MST', 'Masters Thesis'), ('ONL', 'Online'), ('PRC', 'Practicum'), ('REC', 'Recitation'), ('SEM', 'Seminar'), ('SRT', 'Senior Thesis'), ('STU', 'Studio')], db_index=True, help_text='The section activity, e.g. `LEC` for CIS-120-001 (2020A). Options and meanings: <table width=100%><tr><td>""</td><td>"Undefined"</td></tr><tr><td>"CLN"</td><td>"Clinic"</td></tr><tr><td>"CRT"</td><td>"Clinical Rotation"</td></tr><tr><td>"DAB"</td><td>"Dissertation Abroad"</td></tr><tr><td>"DIS"</td><td>"Dissertation"</td></tr><tr><td>"DPC"</td><td>"Doctoral Program Exchange"</td></tr><tr><td>"FLD"</td><td>"Field Work"</td></tr><tr><td>"HYB"</td><td>"Hybrid"</td></tr><tr><td>"IND"</td><td>"Independent Study"</td></tr><tr><td>"LAB"</td><td>"Lab"</td></tr><tr><td>"LEC"</td><td>"Lecture"</td></tr><tr><td>"MST"</td><td>"Masters Thesis"</td></tr><tr><td>"ONL"</td><td>"Online"</td></tr><tr><td>"PRC"</td><td>"Practicum"</td></tr><tr><td>"REC"</td><td>"Recitation"</td></tr><tr><td>"SEM"</td><td>"Seminar"</td></tr><tr><td>"SRT"</td><td>"Senior Thesis"</td></tr><tr><td>"STU"</td><td>"Studio"</td></tr></table>', max_length=50),
+            model_name="section",
+            name="activity",
+            field=models.CharField(
+                choices=[
+                    ("", "Undefined"),
+                    ("CLN", "Clinic"),
+                    ("CRT", "Clinical Rotation"),
+                    ("DAB", "Dissertation Abroad"),
+                    ("DIS", "Dissertation"),
+                    ("DPC", "Doctoral Program Exchange"),
+                    ("FLD", "Field Work"),
+                    ("HYB", "Hybrid"),
+                    ("IND", "Independent Study"),
+                    ("LAB", "Lab"),
+                    ("LEC", "Lecture"),
+                    ("MST", "Masters Thesis"),
+                    ("ONL", "Online"),
+                    ("PRC", "Practicum"),
+                    ("REC", "Recitation"),
+                    ("SEM", "Seminar"),
+                    ("SRT", "Senior Thesis"),
+                    ("STU", "Studio"),
+                ],
+                db_index=True,
+                help_text='The section activity, e.g. `LEC` for CIS-120-001 (2020A). Options and meanings: <table width=100%><tr><td>""</td><td>"Undefined"</td></tr><tr><td>"CLN"</td><td>"Clinic"</td></tr><tr><td>"CRT"</td><td>"Clinical Rotation"</td></tr><tr><td>"DAB"</td><td>"Dissertation Abroad"</td></tr><tr><td>"DIS"</td><td>"Dissertation"</td></tr><tr><td>"DPC"</td><td>"Doctoral Program Exchange"</td></tr><tr><td>"FLD"</td><td>"Field Work"</td></tr><tr><td>"HYB"</td><td>"Hybrid"</td></tr><tr><td>"IND"</td><td>"Independent Study"</td></tr><tr><td>"LAB"</td><td>"Lab"</td></tr><tr><td>"LEC"</td><td>"Lecture"</td></tr><tr><td>"MST"</td><td>"Masters Thesis"</td></tr><tr><td>"ONL"</td><td>"Online"</td></tr><tr><td>"PRC"</td><td>"Practicum"</td></tr><tr><td>"REC"</td><td>"Recitation"</td></tr><tr><td>"SEM"</td><td>"Seminar"</td></tr><tr><td>"SRT"</td><td>"Senior Thesis"</td></tr><tr><td>"STU"</td><td>"Studio"</td></tr></table>',
+                max_length=50,
+            ),
         ),
     ]
