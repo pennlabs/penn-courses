9e224f6b88e1c396df7d640d03eec63f2137d57e
aadalal <57609353+AaDalal@users.noreply.github.com>
2024-04-07 11:06:34 -0400

    add website title to PDP

--- frontend/degree-plan/pages/_app.tsx
+++ frontend/degree-plan/pages/_app.tsx
@@ -5,10 +5,15 @@ import { inter } from "@/fonts";
 import type { AppProps } from "next/app";
 import React from "react";
 import { Theme } from "@radix-ui/themes";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <main className={inter.className}>
+      <Head>
+        <title>Penn Degree Plan</title>
+        <meta name="description" content="Penn Degree Plan by Penn Labs" />
+      </Head>
       <Component {...pageProps} />
     </main>
   );
