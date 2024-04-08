2ba26d292592f0c7cc838215bd8f9fe72ba4fd8f
Daniel Zhao <dz7379@gmail.com>
2024-04-08 03:29:31 -0400

    lint

--- backend/courses/management/commands/recompute_topics.py
+++ backend/courses/management/commands/recompute_topics.py
@@ -157,7 +157,6 @@ class Command(BaseCommand):
         )
         recompute_topics(min_semester, verbose=True, allow_null_parent_topic=bool(min_semester))
         recompute_historical_semester_probabilities(current_semester=semesters[-1], verbose=True)
-            
 
 
 def recompute_historical_semester_probabilities(current_semester, verbose=False):
