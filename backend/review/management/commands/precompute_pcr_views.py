82463ca16b8da73607b185392ca346cc4dbea032
Shiva Menta <shiva.123.menta@gmail.com>
2024-04-24 00:57:46 -0400

    Modify Command for Course Selection

--- backend/review/management/commands/precompute_pcr_views.py
+++ backend/review/management/commands/precompute_pcr_views.py
@@ -18,10 +18,11 @@ def precompute_pcr_views(verbose=False, is_new_data=False):
     responses = CachedReviewResponse.objects.all()
     topic_set = {response.topic_id: response for response in responses}
 
+    missing_objects = []
     objs_to_insert = []
     objs_to_update = []
     cache_deletes = set()
-    same_count = has_count = total_count = 0
+    has_count = total_count = 0
 
     # Iterate through all topics.
     with transaction.atomic():
@@ -34,20 +35,25 @@ def precompute_pcr_views(verbose=False, is_new_data=False):
             total_count += 1
 
             if topic_id in topic_set:
-                has_count += 1
-                same_count += 1
-                response_obj = topic_set[topic_id]
-                if is_new_data:
-                    same_count -= 1
-                    response_obj.response = manual_course_reviews(
-                        course_code_list[0], topic.most_recent.semester
+                try:
+                    has_count += 1
+                    response_obj = topic_set[topic_id]
+                    if is_new_data:
+                        response_obj.response = manual_course_reviews(
+                            topic.most_recent.full_code, topic.most_recent.semester
+                        )
+                        cache_deletes.add(topic_id)
+                    response_obj.expired = False
+                    objs_to_update.append(response_obj)
+                except Http404:
+                    missing_objects.append((topic_id, topic.most_recent.full_code))
+                    logging.info(
+                        f"Topic returned 404 (topic_id {topic_id}, "
+                        f"course_code {course_code_list[0]}, semester {topic.most_recent.semester})"
                     )
-                    cache_deletes.add(topic_id)
-                response_obj.expired = False
-                objs_to_update.append(response_obj)
             else:
                 try:
-                    review_json = manual_course_reviews(course_code_list[0], topic.most_recent.semester)
+                    review_json = manual_course_reviews(topic.most_recent.full_code, topic.most_recent.semester)
                     objs_to_insert.append(
                         CachedReviewResponse(
                             topic_id=topic_id, response=review_json, expired=False
@@ -59,13 +65,14 @@ def precompute_pcr_views(verbose=False, is_new_data=False):
                             cache_deletes.add(curr_topic_id)
                         cache_deletes.add(course_code)
                 except Http404:
+                    missing_objects.append((topic_id, topic.most_recent.full_code))
                     logging.info(
                         f"Topic returned 404 (topic_id {topic_id}, "
                         f"course_code {course_code_list[0]}, semester {topic.most_recent.semester})"
                     )
 
         if verbose:
-            print(f"{total_count} course reviews updated, {has_count} of which were already in the database. Only {total_count - same_count} course reviews were updated.")
+            print(f"{total_count} course reviews covered, {has_count} of which were already in the database. {len(objs_to_insert)} course reviews were created. {len(objs_to_update)} course reviews were updated.")
 
         # Bulk create / update objects.
         if verbose:
@@ -79,9 +86,15 @@ def precompute_pcr_views(verbose=False, is_new_data=False):
         CachedReviewResponse.objects.filter(expired=True).delete()
         cache.delete_many(cache_deletes)
 
+        print(missing_objects)
 
 class Command(BaseCommand):
     help = "Precompute PCR views for all topics"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--new_data", action="store_false", help="Include this flag to recalculate review data."
+        )
+
     def handle(self, *args, **kwargs):
-        precompute_pcr_views(verbose=True, is_new_data=True)
+        precompute_pcr_views(verbose=True, is_new_data=kwargs["new_data"])
