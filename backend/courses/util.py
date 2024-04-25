0fafac29faa75e788c65167707738297cdc14ac7
Shiva Menta <shiva.123.menta@gmail.com>
2024-04-24 21:45:18 -0400

    Intermediate Progress

--- backend/courses/util.py
+++ backend/courses/util.py
@@ -712,3 +712,13 @@ def get_semesters(semesters: str = None) -> list[str]:
             if s not in possible_semesters:
                 raise ValueError(f"Provided semester {s} was not found in the db.")
     return sorted(semesters)
+
+def get_section_from_course_professor_semester(course_code, professor, semester):
+    """
+    Attempts to return a course section that matches the given parameters.
+    ValueError is raised if the section does not exist.
+    """
+    # a section has multiple professors
+    section = Section.objects.annotate("course__full_code").filter(course__full_code=course_code, ).order_by("code").first()
+    return None
+    raise ValueError(f"No section exists with course code ({course_code}), professor ({professor}), semester ({semester})")
