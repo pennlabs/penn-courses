06825241007bcf2e8eaea6144a2e63b0a9140226
luke-rt <luke.rtong@gmail.com>
2024-10-27 13:07:36 -0400

    Nextjs route rewrites

--- frontend/review-nextjs/next.config.mjs
+++ frontend/review-nextjs/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "http://127.0.0.1:8000/:path*",
+            },
+            {
+                source: "/accounts/:path*",
+                destination: "http://127.0.0.1:8000/:path*",
+            },
+        ];
+    },
+};
 
 export default nextConfig;
