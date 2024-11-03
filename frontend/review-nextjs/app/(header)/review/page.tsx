a0090bc0c99a2f0a68d3f20565955a9be027e7c2
luke-rt <luke.rtong@gmail.com>
2024-11-03 13:40:13 -0500

    updated Next.js version, rewrites

---
+++ frontend/review-nextjs/app/(header)/review/page.tsx
@@ -0,0 +1,8 @@
+import { cn } from "@/lib/utils";
+export default function Review() {
+    return (
+        <div id="review" className={cn("mx-[20%]")}>
+            Secret review data
+        </div>
+    );
+}
--- frontend/review-nextjs/lib/utils.ts
+++ frontend/review-nextjs/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * @returns {string | boolean} The CSRF token used by the Django REST Framework
+ */
+const getCsrf = (): string | boolean => {
+  return document.cookie &&
+    document.cookie
+        .split("; ")
+        .reduce(
+            (acc: string | boolean, cookie) =>
+                acc ||
+                (cookie.substring(0, "csrftoken".length + 1) ===
+                    "csrftoken=" &&
+                    decodeURIComponent(
+                        cookie.substring("csrftoken=".length)
+                    )),
+            false
+        );
+};
+
+export default getCsrf;
