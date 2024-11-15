1ad94a1baac493a5f2411eba0274b5b664f38e7b
Samuel Lao <samuel.lao100@gmail.com>
2024-11-15 22:18:13 +0000

    please work

--- frontend/degree-plan/pages/index.tsx
+++ frontend/degree-plan/pages/index.tsx
@@ -29,21 +29,7 @@ export default function Home() {
           value={{
             fetcher: (resource, init) =>
             {
-<<<<<<< HEAD
               return fetch(resource, init).then((res) => res.json())
-=======
-              console.log(resource)
-              if (typeof resource === "string") {
-                return fetch(resource, init).then((res) => res.json())
-              }
-              if (Array.isArray(resource) && resource.every(k => typeof k === "string")) {
-                const last = resource.at(-1)
-                if(typeof last === "string") {
-                  return fetch(last, init).then((res) => res.json())
-                }
-              }
-              throw new Error("Undefined behavior due to invalid key")
->>>>>>> ec732db7 (fixed some parsing issues)
             },
             // fetch(resource, init).then((res) => res.json()),
             provider: () => new Map(),
