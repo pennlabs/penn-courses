a01fc9220b24a7969b67325c06c6112ecc1d5bbf
luke-rt <luke.rtong@gmail.com>
2025-01-23 16:37:20 -0500

    Removed old auth stuff

--- frontend/review-nextjs/app/(header)/(auth)/review/page.tsx
+++ frontend/review-nextjs/app/(header)/(auth)/review/page.tsx
@@ -1,35 +1,20 @@
 "use client";
-import Skeleton from "@/components/Skeleton";
 import { cn } from "@/lib/utils";
-import { useEffect } from "react";
-import { useAuth } from "react-oidc-context";
-
-/*
-On error with fetching data
-try {
-    userManager.signinSilent()
-} catch (error) {
-    userManager.signinRedirect()
-}
-*/
 
 export default function Review() {
-    const auth = useAuth();
-
     return (
         <div id="review" className={cn("mx-[20%]")}>
-            {auth.isLoading ? <Skeleton /> : <ReviewPage />}
+            {/* {auth.isLoading ? <Skeleton /> : <ReviewPage />} */}
         </div>
     );
 }
 
 function ReviewPage() {
-    const auth = useAuth();
-
     return (
         <p>
-            Secret data that needs authentication {auth.isAuthenticated}{" "}
-            {auth.user?.profile.email}
+            Secret data
+            {/* Secret data that needs authentication {auth.isAuthenticated}{" "} */}
+            {/* {auth.user?.profile.email} */}
         </p>
     );
 }
