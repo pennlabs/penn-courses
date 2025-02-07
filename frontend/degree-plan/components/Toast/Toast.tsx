6f8113d61643c9c3d644dc6c9c0123046e6858b8
Samuel Lao <samuel.lao100@gmail.com>
2025-02-07 21:57:25 +0000

    quick fix

--- frontend/degree-plan/components/Toast/Toast.tsx
+++ frontend/degree-plan/components/Toast/Toast.tsx
@@ -1,20 +1,6 @@
 import { createContext } from "react";
 import { toast } from "react-toastify";
 
-
-function showToast(text: string, error: boolean) {
-    if (error) {
-      toast.error(text, {
-        position: toast.POSITION.BOTTOM_CENTER,
-      });
-    } else {
-      toast.success(text, {
-        position: toast.POSITION.BOTTOM_CENTER,
-      });
-    }
-  }
-  
-  
   
 const ToastContext = createContext((text: string, error: boolean) => {});
 
