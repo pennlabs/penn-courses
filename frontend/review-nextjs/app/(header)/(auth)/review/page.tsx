7ed3316b2770e4051ad79bb4825d1f1d876287e1
luke-rt <luke.rtong@gmail.com>
2025-02-10 15:10:50 -0500

    Made some progress with nextauth and middleware

--- frontend/review-nextjs/app/(header)/(auth)/review/page.tsx
+++ frontend/review-nextjs/app/(header)/(auth)/review/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { cn } from "@/lib/utils";
 
 export default function Review() {
@@ -9,15 +8,5 @@ export default function Review() {
             <p>Static data</p>
             {/* {session ? <ReviewPage /> : <p>Not authenticated</p>} */}
         </div>
-    );
-}
-
-function ReviewPage() {
-    return (
-        <p>
-            Secret static data
-            {/* Secret data that needs authentication {auth.isAuthenticated}{" "} */}
-            {/* {auth.user?.profile.email} */}
-        </p>
     );
 }
--- frontend/review-nextjs/middleware.ts
+++ frontend/review-nextjs/middleware.ts
@@ -1,12 +1,27 @@
 import { withAuth } from "next-auth/middleware";
+import { redirect } from "next/dist/server/api-utils";
+import { NextResponse } from "next/server";
 
-export default withAuth({
+export default withAuth(
+  // only executed after authentications
+  function middleware(req) {
+    console.log(req);
+  },
+  {
   pages: {
-    signIn: "/auth/signin",
-    signOut: "/auth/signout",
+    signIn: "/accounts/login",
+    signOut: "/accounts/logout",
     error: "/auth/error",
     verifyRequest: "/auth/verify-request",
+  },
+  callbacks: {
+    authorized({ req, token }) {
+      console.log(req)
+      return !!token;
     },
+  }
 });
 
-export const config = { matcher: ["/review"] }
+export const config = {
+  matcher: ["/review"],
+}
