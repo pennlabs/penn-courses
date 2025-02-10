0c9235a86d099a269b76e62ab310870f3bca8863
luke-rt <luke.rtong@gmail.com>
2025-02-10 16:04:21 -0500

    Trying without nextauth

--- frontend/review-nextjs/middleware.ts
+++ frontend/review-nextjs/middleware.ts
@@ -1,27 +1,17 @@
-import { withAuth } from "next-auth/middleware";
-import { redirect } from "next/dist/server/api-utils";
-import { NextResponse } from "next/server";
-
-export default withAuth(
-  // only executed after authentications
-  function middleware(req) {
-    console.log(req);
-  },
-  {
-  pages: {
-    signIn: "/accounts/login",
-    signOut: "/accounts/logout",
-    error: "/auth/error",
-    verifyRequest: "/auth/verify-request",
-  },
-  callbacks: {
-    authorized({ req, token }) {
-      console.log(req)
-      return !!token;
-    },
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const sessionid = request.cookies.get('sessionid')?.value;
+  const csrftoken = request.cookies.get('csrftoken')?.value;
+  if (!sessionid || !csrftoken) {
+    return NextResponse.redirect(new URL(`/accounts/login/?next=${request.nextUrl.pathname}`, request.url));
   }
-});
-
+  return NextResponse.next();
+}
+ 
+// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/review"],
+  matcher: '/review',
 }
--- frontend/review-nextjs/next.config.mjs
+++ frontend/review-nextjs/next.config.mjs
@@ -3,12 +3,12 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: "/api/:path*",
-                destination: "http://127.0.0.1:8000/:path*",
+                source: "/api/:apipath*",
+                destination: "http://127.0.0.1:8000/api/:apipath*",
             },
             {
                 source: "/accounts/:path*",
-                destination: "http://127.0.0.1:8000/accounts/:path*",
+                destination: "http://127.0.0.1:8000/accounts/:path*/",
             },
         ];
     },
