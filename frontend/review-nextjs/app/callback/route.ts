a2cb674f7a56387905da80ed8239c9e84d9a0178
luke-rt <luke.rtong@gmail.com>
2025-02-12 21:32:52 -0500

    JWT token logic working, need to cryptographically verify

---
+++ frontend/review-nextjs/app/callback/route.ts
@@ -0,0 +1,50 @@
+import {
+    OIDC_TOKEN_ENDPOINT,
+    OIDC_CLIENT_ID,
+    OIDC_REDIRECT_URI,
+    OIDC_CLIENT_SECRET,
+} from "@/lib/api";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const code = request.nextUrl.searchParams.get("code");
+    if (!code) {
+        return NextResponse.redirect(new URL("/", request.url), {
+            status: 401,
+        });
+    }
+
+    const res = await fetch(OIDC_TOKEN_ENDPOINT, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            grant_type: "authorization_code",
+            client_id: OIDC_CLIENT_ID,
+            client_secret: OIDC_CLIENT_SECRET,
+            redirect_uri: OIDC_REDIRECT_URI,
+            code,
+        }).toString(),
+    });
+
+    if (!res.ok) {
+        return NextResponse.redirect(new URL("/", request.url), {
+            status: 401,
+        });
+    }
+
+    const tokens = await res.json();
+
+    const response = NextResponse.redirect(new URL("/review", request.url));
+    response.cookies.set("refresh_token", tokens.id_token, {
+        httpOnly: true,
+        secure: true,
+    });
+    response.cookies.set("access_token", tokens.access_token, {
+        httpOnly: true,
+        secure: true,
+    });
+
+    return response;
+}
--- frontend/review-nextjs/middleware.ts
+++ frontend/review-nextjs/middleware.ts
@@ -1,17 +1,67 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  const sessionid = request.cookies.get('sessionid')?.value;
-  const csrftoken = request.cookies.get('csrftoken')?.value;
-  if (!sessionid || !csrftoken) {
-    return NextResponse.redirect(new URL(`/accounts/login/?next=${request.nextUrl.pathname}`, request.url));
-  }
-  return NextResponse.next();
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import {
+    JWKS_URI,
+    OIDC_AUTHORIZATION_ENDPOINT,
+    OIDC_CLIENT_ID,
+    OIDC_REDIRECT_URI,
+} from "@/lib/api";
+import * as jose from "jose";
+
+interface RawPayload {
+    sub: string;
+    name: string;
+    email: string;
+    pennkey: string;
+    pennid: number;
+    is_staff: boolean;
+    is_active: boolean;
 }
- 
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+    const refresh_token = request.cookies.get("refresh_token")?.value;
+    const access_token = request.cookies.get("access_token")?.value;
+
+    if (access_token) {
+        // Verify access_token
+        const jwks = await jose.createRemoteJWKSet(new URL(JWKS_URI));
+        try {
+            const { payload } = await jose.jwtVerify<RawPayload>(
+                access_token,
+                jwks,
+                {
+                    clockTolerance: "1m",
+                }
+            );
+
+            console.log(payload);
+            return NextResponse.next();
+        } catch (e) {
+            console.error(e);
+            return NextResponse.redirect(new URL("/", request.url), {
+                status: 401,
+            });
+        }
+    }
+    // null/invalid access token
+
+    if (refresh_token) {
+        // Request access_token from platform
+        return NextResponse.next(); // temp
+    }
+
+    // Request authorization code
+    const authorizationUrl = new URL(OIDC_AUTHORIZATION_ENDPOINT);
+    authorizationUrl.searchParams.set("client_id", OIDC_CLIENT_ID);
+    authorizationUrl.searchParams.set("redirect_uri", OIDC_REDIRECT_URI);
+    authorizationUrl.searchParams.set("response_type", "code");
+    authorizationUrl.searchParams.set("scope", "openid read");
+
+    return NextResponse.redirect(authorizationUrl.toString());
+}
+
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/review',
-}
+    matcher: ["/review"],
+};
