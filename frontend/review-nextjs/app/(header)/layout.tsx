14e62efa3e4a21eb068c9f9cc78f6d044d32f0c4
luke-rt <luke.rtong@gmail.com>
2025-02-12 21:32:52 -0500

    JWT token logic working, need to cryptographically verify

--- frontend/review-nextjs/app/(header)/layout.tsx
+++ frontend/review-nextjs/app/(header)/layout.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/Header";
-import { cn } from "@/lib/utils";
 
 export default function RootLayout({
     children,
--- frontend/review-nextjs/lib/api.ts
+++ frontend/review-nextjs/lib/api.ts
@@ -1,19 +1,23 @@
-export const BASE_URL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:3000'
-  : process.env.NEXT_PUBLIC_BASE_URL;
+export const BASE_URL =
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : process.env.NEXT_PUBLIC_BASE_URL!;
 
-export const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID ?? "";
-export const CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET ?? "";
+export const OIDC_CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID!;
+export const OIDC_CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET!;
+export const OIDC_REDIRECT_URI = `${BASE_URL}/callback`;
+export const OIDC_AUTHORITY = "https://platform.pennlabs.org";
+export const OIDC_AUTHORIZATION_ENDPOINT = `${OIDC_AUTHORITY}/accounts/authorize/`;
+export const OIDC_TOKEN_ENDPOINT = `${OIDC_AUTHORITY}/accounts/token/`;
+export const JWKS_URI = `${OIDC_AUTHORITY}/accounts/.well-known/jwks.json`;
 
 export const doAPIRequest = (path: string, options = {}): Promise<Response> =>
-  fetch(`/api${path}`, options);
+    fetch(`/api${path}`, options);
 
 export function getLogoutUrl(): string {
-  return `/accounts/logout/?next=${encodeURIComponent(
-    `${BASE_URL}/logout`
-  )}`;
+    return `/accounts/logout/?next=${encodeURIComponent(`${BASE_URL}/logout`)}`;
 }
 
 export function getLoginUrl() {
-  return `/accounts/login/?next=${BASE_URL}`;
+    return `/accounts/login/?next=${BASE_URL}`;
 }
