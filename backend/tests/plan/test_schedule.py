c01206f31cea8f6c01bb219228b13b1d73505591
Daniel Zhao <dz7379@gmail.com>
2025-08-19 12:13:17 -0500

    Update test_schedule.py

--- backend/tests/plan/test_schedule.py
+++ backend/tests/plan/test_schedule.py
@@ -1241,12 +1241,9 @@ class BreakViewSetTests(TestCase):
         )
         self.break_obj.save()
 
-        # Use Django's reverse to dynamically get the correct endpoint
-        self.break_list_url = reverse("breaks")  # /api/plan/breaks/
-        self.break_detail_url = reverse(
-            "breaks", kwargs={"pk": self.break_obj.id}
-        )  # /api/plan/breaks/{id}/
-
+        self.break_list_url = "/api/plan/breaks/" 
+        self.break_detail_url = "/api/plan/breaks/{}/".format(self.break_obj.id)
+            
     @patch("plan.views.set_meetings")
     def test_create_break(self, mock_set_meetings):
         """
