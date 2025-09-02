e1d1e34a60e577d576055eb3d6561f5d23cd9071
Daniel Zhao <68676272+el-agua@users.noreply.github.com>
2025-09-01 23:43:54 -0400

    Refactor breaks to persist across schedules (#725)
    * Update util.py
    
    * Refactor breaks to persist across schedules
    
    * lint

---
+++ frontend/plan/reducers/breaks.js
@@ -0,0 +1,31 @@
+import { ADD_BREAK, REMOVE_BREAK } from "../actions";
+
+const initialState = {
+    breaks: [],
+    loading: false,
+};
+
+export const breaks = (state = initialState, { type, ...action }) => {
+    switch (type) {
+        case ADD_BREAK:
+            if (!action.breakItem) {
+                return state; // No break item to add
+            }
+            if (state.breaks.some((b) => b.id === action.breakItem.id)) {
+                return state; // Break already exists
+            }
+            return {
+                ...state,
+                breaks: [...state.breaks, action.breakItem],
+            };
+        case REMOVE_BREAK:
+            return {
+                ...state,
+                breaks: state.breaks.filter((b) => b.id !== action.id),
+            };
+        default:
+            return {
+                ...state,
+            };
+    }
+};
