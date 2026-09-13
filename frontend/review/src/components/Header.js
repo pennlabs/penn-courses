a272dda4f07c076d0aa5a9765d09184c9de31875
JonKach <jon.kachappilly@gmail.com>
2026-09-13 18:26:33 +0000

    fixed lint perhaps?

--- frontend/review/src/components/Header.js
+++ frontend/review/src/components/Header.js
@@ -112,7 +112,7 @@ const Header = () => {
   const dispatch = useFilterDispatch();
 
   const getCourseCount = () =>
-    Object.keys(localStorage).filter((a) => !a.startsWith("meta-")).length;
+    Object.keys(localStorage).filter(a => !a.startsWith("meta-")).length;
   const [courseCount, setCourseCount] = useState(getCourseCount());
 
   useEffect(() => {
@@ -136,7 +136,7 @@ const Header = () => {
             alignItems: "center",
             gap: "15px",
             margin: "0 28px",
-            cursor: "pointer",
+            cursor: "pointer"
           }}
           onClick={() => {
             if (window.location.pathname === "/") {
